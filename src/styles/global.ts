a96bae9eeddfdafce8b4e80ecb749ac447985fbd
Yoo TaeSeung <remicon99@gmail.com>
2024-07-04 15:10:28 +0900

    setting: 모바일 및 데스크탑 뷰포트를 위한 글로벌 스타일 설정

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,26 +1,49 @@
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
 
-import Reset from "./reset";
+import Reset from './reset';
 
 const GlobalStyle = css`
   ${Reset}
   * {
     box-sizing: border-box;
 
-    font-family: "Pretendard Variable", Pretendard, -apple-system,
-      BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI",
-      "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic",
-      "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui,
+      Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
+      'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
   }
 
-  html {
+  :root {
+    --min-width: 375px;
+    --max-width: 600px;
+  }
+
+  html,
+  body {
     font-size: 62.5%;
     scrollbar-width: none;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
   #root {
-    min-width: 37.5rem;
+    width: 100%;
+    min-width: var(--min-width);
+    max-width: var(--max-width);
     min-height: 100vh;
+    background-color: #fff;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 600px) {
+    #root {
+      max-width: var(--max-width);
+      box-shadow: 0 0 2px rgba(0, 0, 0, 0.4);
+    }
   }
 
   ::-webkit-scrollbar {
