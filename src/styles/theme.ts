4570480bcfcaa997e159d684cab09002c1760700
Yoo TaeSeung <remicon99@gmail.com>
2024-07-05 00:47:26 +0900

    setting: 글로벌 폰트 수정 (#1)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,94 +1,106 @@
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
 
 const theme = {
   color: {
-    white: "#FFFFFF",
-    background: "#F8F9FC",
-    lightgray1: "#DFE2E7",
-    lightgray2: "#C6CAD4",
-    midgray1: "#9FA4AE",
-    midgray2: "#6B6F77",
-    darkgray: "#3E3F45",
-    blackgray: "#373737",
-    black: "#17171A",
+    white: '#FFFFFF',
+    background: '#F8F9FC',
+    lightgray1: '#DFE2E7',
+    lightgray2: '#C6CAD4',
+    midgray1: '#9FA4AE',
+    midgray2: '#6B6F77',
+    darkgray: '#3E3F45',
+    blackgray: '#373737',
+    black: '#17171A',
 
-    purple1: "#5451FF",
-    purple2: "#7774FF",
-    purple3: "#9997FF",
-    purple4: "#BBB9FF",
-    purple5: "#DDDCFF",
-    purple6: "#EEEEFF",
+    purple1: '#5451FF',
+    purple2: '#7774FF',
+    purple3: '#9997FF',
+    purple4: '#BBB9FF',
+    purple5: '#DDDCFF',
+    purple6: '#EEEEFF',
 
-    error: "#FF4668",
+    error: '#FF4668',
   },
   font: {
-    "head01-b-22": css`
+    'head01-b-22': css`
       font-size: 22px;
       font-weight: 700;
       line-height: 145%;
       letter-spacing: 0px;
     `,
-    "head02-b-20": css`
+    'head02-b-20': css`
       font-size: 20px;
       font-weight: 700;
       line-height: normal;
       letter-spacing: 0px;
     `,
-    "head03-b-18": css`
+    'head03-b-18': css`
       font-size: 18px;
       font-weight: 700;
       line-height: normal;
       letter-spacing: 0px;
     `,
-    "head04-b-16": css`
+    'head04-b-16': css`
       font-size: 16px;
       font-weight: 700;
       line-height: normal;
       letter-spacing: 0px;
     `,
-    "subhead01-sb-18": css`
+    'subhead01-sb-18': css`
       font-size: 18px;
       font-weight: 600;
       line-height: normal;
       letter-spacing: 0px;
     `,
-    "subhead02-sb-16": css`
+    'subhead02-sb-16': css`
       font-size: 16px;
       font-weight: 600;
-      line-height: normal;
+      line-height: 140%;
       letter-spacing: 0px;
     `,
-    "subhead03-m-16": css`
+    'subhead03-m-16': css`
       font-size: 16px;
       font-weight: 500;
-      line-height: normal;
+      line-height: 140%;
       letter-spacing: 0px;
     `,
-    "subhead04-sb-14": css`
+    'subhead04-sb-15': css`
+      font-size: 15px;
+      font-weight: 600;
+      line-height: 140%;
+      letter-spacing: 0px;
+    `,
+    'subhead05-sb-14': css`
       font-size: 14px;
       font-weight: 600;
       line-height: 140%;
       letter-spacing: 0px;
     `,
-    "body01-r-14": css`
+    'body01-r-15': css`
+      font-size: 15px;
+      font-weight: 400;
+      line-height: 140%;
+      letter-spacing: 0px;
+    `,
+    'body02-r-14': css`
       font-size: 14px;
       font-weight: 400;
       line-height: 140%;
       letter-spacing: 0px;
     `,
-    "body02-r-12": css`
+    'body03-r-12': css`
       font-size: 12px;
       font-weight: 400;
       line-height: normal;
       letter-spacing: 0px;
     `,
-    "body03-m-12": css`
+    'body04-m-12': css`
       font-size: 12px;
       font-weight: 500;
       line-height: normal;
       letter-spacing: 0px;
     `,
-    "body04-r-10": css`
+    'body05-r-10': css`
       font-size: 10px;
       font-weight: 400;
       line-height: 140%;
