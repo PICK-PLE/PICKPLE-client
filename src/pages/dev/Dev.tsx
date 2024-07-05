36377fda6b4bb02c640ba579a0b7a0f521aa8d78
imhwarang <imhwarang4@naver.com>
2024-07-06 02:42:52 +0900

    refactor: button 공통 폴더 생성 및 totalPrice 속성값 추가

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,11 +1,11 @@
-import PayButton from 'src/components/common/PayButton/PayButton';
+import PayButton from 'src/components/common/button/PayButton/PayButton';
 import { payButtonsStyle } from './Dev.style';
 
 const Dev = () => {
   return (
     <div css={payButtonsStyle}>
-      <PayButton variant="kakao" />
-      <PayButton variant="toss" />
+      <PayButton variant="kakao" totalPrice={10000} />
+      <PayButton variant="toss" totalPrice={10000} />
     </div>
   );
 };
