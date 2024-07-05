51e8a915d75b71c567e13bfbbdf83fb6ff4e970b
Yoo TaeSeung <remicon99@gmail.com>
2024-07-06 05:21:00 +0900

    setting: 프로젝트 폰트 설정 (Pretendard) (#17)

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,10 +6,6 @@ const GlobalStyle = css`
   ${Reset}
   * {
     box-sizing: border-box;
-
-    font-family: 'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui,
-      Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
-      'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
   }
 
   :root {
