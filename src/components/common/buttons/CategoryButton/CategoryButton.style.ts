15b9cc72b6a1bfeab8b00fcc8027baae7987cc30
장정안 <wjddks96@naver.com>
2024-07-06 19:48:04 +0900

    feat: CategoryButton 컴포넌트 구현

---
+++ src/components/common/buttons/CategoryButton/CategoryButton.style.ts
@@ -0,0 +1,49 @@
+import { Theme, css } from '@emotion/react';
+import { flexGenerator } from '@styles/generator';
+
+export const categoryButtonContainer = css`
+  ${flexGenerator('column')};
+  width: 5.7rem;
+  gap: 0.5rem;
+`;
+export const categoryButtonWrapper = (theme: Theme, isSelected: boolean) => css`
+  ${flexGenerator('column')};
+
+  height: 5.7rem;
+  align-self: stretch;
+
+  border-radius: 10px;
+  background: ${isSelected ? theme.color.purple5 : theme.color.background};
+  border: ${isSelected ? `1px solid ${theme.color.purple2}` : 'none'};
+
+  color: ${theme.color.lightgray2};
+
+  cursor: pointer;
+`;
+
+export const categoryButtonStyle = (theme: Theme, isSelected: boolean) => css`
+  border: none;
+  background: none;
+  cursor: pointer;
+
+  & span {
+    fill: ${isSelected ? theme.color.purple2 : theme.color.darkgray}; /* SVG 색상 변경 */
+  }
+`;
+
+export const iconWrapper = (theme: Theme, isSelected: boolean) => css`
+  ${flexGenerator()}
+  width: 3.6rem;
+  height: 3.6rem;
+
+  & svg {
+    path {
+      stroke: ${isSelected ? theme.color.purple2 : ''};
+    }
+  }
+`;
+
+export const textStyle = (theme: Theme) => css`
+  ${theme.color.lightgray2};
+  ${theme.font['body04-m-12']}
+`;
---
+++ src/components/common/buttons/CategoryButton/CategoryButton.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { HTMLAttributes } from 'react';
+import {
+  categoryButtonWrapper,
+  categoryButtonStyle,
+  iconWrapper,
+  textStyle,
+  categoryButtonContainer,
+} from './CategoryButton.style';
+import { useTheme } from '@emotion/react';
+
+interface CategoryButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  icon?: React.ReactNode;
+}
+
+const CategoryButton = ({ children, onSelect, onClick, icon }: CategoryButtonProps) => {
+  const theme = useTheme();
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setIsSelected(!isSelected);
+    if (onClick) {
+      onClick(event);
+    }
+  };
+
+  return (
+    <section css={categoryButtonContainer}>
+      <div css={(theme) => [categoryButtonWrapper(theme, isSelected)]}>
+        <button
+          type="button"
+          onSelect={onSelect}
+          onClick={handleClick}
+          css={(theme) => [categoryButtonStyle(theme, isSelected)]}>
+          <span css={iconWrapper(theme, isSelected)}>{icon}</span>
+        </button>
+      </div>
+      <p css={textStyle}>{children}</p>
+    </section>
+  );
+};
+
+export default CategoryButton;
