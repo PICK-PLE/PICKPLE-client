5141c203380c735fae3dea063dc53f4817c732bc
장정안 <wjddks96@naver.com>
2024-07-06 18:42:26 +0900

    style: css 속성 순서 정렬

--- src/components/common/Label/Label.style.ts
+++ src/components/common/Label/Label.style.ts
@@ -8,33 +8,51 @@ export const labelStyle = css`
 
 export const labelVariant = (theme: Theme) => ({
   status: css`
-    border-radius: 5px;
-    background-color: ${theme.color.purple2};
+    display: inline-flex;
     width: 5.7rem;
     height: 2rem;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    background-color: ${theme.color.purple2};
     color: ${theme.color.white};
     ${theme.font['body03-r-12']}
   `,
   dDay: css`
     display: inline-flex;
+    padding: 0.4rem 1rem;
+    align-items: center;
+    justify-content: center;
     border-radius: 5px;
     background-color: ${theme.color.purple2};
     color: ${theme.color.white};
     ${theme.font['subhead05-sb-14']}
-    padding: 0.4rem 1rem;
     white-space: nowrap;
   `,
-
   category: css`
-    ${flexGenerator()}
+    display: flex;
     width: 10.7rem;
     height: 3.4rem;
     padding: 0.5rem 1rem;
-    background: ${theme.color.purple6};
+    align-items: center;
+    justify-content: center;
+    gap: 0.4rem;
     border-radius: 5px;
+    background-color: ${theme.color.purple6};
     color: ${theme.color.purple1};
     ${theme.font['body03-r-12']}
+  `,
+  textCount: css`
+    display: inline-flex;
+    height: 3.4rem;
+    padding: 0.6rem 1rem;
+    align-items: center;
+    justify-content: center;
     gap: 0.4rem;
+    border-radius: 5px;
+    background-color: ${theme.color.white};
+    color: ${theme.color.darkgray};
+    ${theme.font['subhead02-sb-16']};
   `,
 });
 
