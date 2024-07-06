c46c3c640aa645f17d49d54efb3fa27a53c91243
Parkchaeyeon <ccyy1029@naver.com>
2024-07-06 19:03:24 +0900

    chore: dev 페이지 내용 지우기

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,11 +1,5 @@
-import NumberLabel from "src/components/common/NumberLabel/NumberLabel";
-
 const Dev = () => {
-  return <div>
-    <NumberLabel>1</NumberLabel>
-    <NumberLabel>2</NumberLabel>
-    <NumberLabel>Q1</NumberLabel>
-  </div>;
+  return <div></div>;
 };
 
 export default Dev;
