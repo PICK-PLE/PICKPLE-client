ff4cc79d9a0f021ac577bce3170414bd2fe74470
장정안 <wjddks96@naver.com>
2024-07-06 19:47:14 +0900

    feat: ic_njob 아이콘 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,4 +1,5 @@
-export { default as IcKakaoLogo } from './IcKakaoLogo';
 export { default as IcKakaoPay } from './IcKakaoPay';
 export { default as IcTossPay } from './IcTossPay';
+export { default as IcKakaoLogo } from './IcKakaoLogo';
+export { default as IcNjob } from './IcNjob';
 export { default as Logo } from './Logo';
