41235ad236c19480880f132d7464ae97074fc030
Yoo TaeSeung <remicon99@gmail.com>
2024-07-06 19:11:09 +0900

    refactor: 컴포넌트 export 추가

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,32 +1,29 @@
 import { IcKakaoLogo, IcCopyPlus } from '@svg';
-import SocialLoginButton from 'src/components/common/buttons/SocialLoginButton/SocialLoginButton';
-import PayButton from 'src/components/common/buttons/PayButton/PayButton';
 import { payButtonsStyle, socialLoginButtonStyle } from './Dev.style';
-import { IconButton } from '@components';
+import { IconButton, SocialLoginButton, PayButton } from '@components';
 
 const Dev = () => {
   return (
     <>
-    <div css={socialLoginButtonStyle}>
-      <SocialLoginButton
-        platform="kakao"
-        icon={<IcKakaoLogo />}
-        onClick={() => console.log('카카오로 시작하기 클릭')}>
-        카카오로 시작하기
-      </SocialLoginButton>
-      <div css={payButtonsStyle}>
-        <PayButton variant="kakao" totalPrice={10000} />
-        <PayButton variant="toss" totalPrice={10000} />
+      <div css={socialLoginButtonStyle}>
+        <SocialLoginButton
+          platform="kakao"
+          icon={<IcKakaoLogo />}
+          onClick={() => console.log('카카오로 시작하기 클릭')}>
+          카카오로 시작하기
+        </SocialLoginButton>
+        <div css={payButtonsStyle}>
+          <PayButton variant="kakao" totalPrice={10000} />
+          <PayButton variant="toss" totalPrice={10000} />
+        </div>
       </div>
-    </div>
-        <div>
-      <IconButton icon={<IcCopyPlus />} onClick={() => {}}>
-        작성하기
-      </IconButton>
-    </div>
+      <div>
+        <IconButton icon={<IcCopyPlus />} onClick={() => {}}>
+          작성하기
+        </IconButton>
+      </div>
     </>
   );
-
 };
 
 export default Dev;
