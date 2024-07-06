86713de27dba90855ed6b8bea6e1a160e404bd6c
장정안 <wjddks96@naver.com>
2024-07-06 18:42:15 +0900

    feat: Label variant에 textCount 추가

--- src/components/common/Label/Label.tsx
+++ src/components/common/Label/Label.tsx
@@ -1,7 +1,7 @@
 import { useTheme } from '@emotion/react';
 import { iconWrapperStyle, labelSize, labelStyle, labelVariant } from './Label.style';
 interface LabelProps {
-  variant: 'status' | 'dDay' | 'category';
+  variant: 'status' | 'dDay' | 'category' | 'textCount';
   size?: 'small' | 'large';
   status?: '입금 대기' | '승인 대기' | '승인 완료' | '승인 거절' | '환불 완료';
   children: React.ReactNode;
