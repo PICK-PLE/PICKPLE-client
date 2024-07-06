227dad203d8a7d0bb850bb591f02f0e9d000c4d7
Parkchaeyeon <ccyy1029@naver.com>
2024-07-06 21:36:58 +0900

    chore: dev 페이지 내용 지우기

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,10 +1,5 @@
-import QuestionText from 'src/components/common/QuestionText/QuestionText';
 const Dev = () => {
-  return (
-    <>
-      <QuestionText numberLabel="Q1">호스트님은 어떤 분이신가요?</QuestionText>
-    </>
-  );
+  return <></>;
 };
 
 export default Dev;
