211e8bbb2efc5f86c95748c10de4382947cccada
imhwarang <imhwarang4@naver.com>
2024-07-07 23:39:03 +0900

    refactor: Dev.tsx 원상복귀

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,13 +1,5 @@
-import { ProgressBar } from '@components';
-import ClipboardCopyButton from 'src/components/common/buttons/ClipboardCopyButton/ClipboardCopyButton';
-
 const Dev = () => {
-  const text = '12313413413';
-  return (
-    <>
-      <ClipboardCopyButton text={text} />
-    </>
-  );
+  return <></>;
 };
 
 export default Dev;
