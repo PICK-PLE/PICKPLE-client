85adac71d81a2710e05e7003453d73fb55e91dad
Parkchaeyeon <ccyy1029@naver.com>
2024-07-07 15:26:03 +0900

    feat: Input에 textLength 관리하는 부분 추가

--- src/components/common/inputs/Input/Input.style.ts
+++ src/components/common/inputs/Input/Input.style.ts
@@ -1,21 +1,27 @@
 import { css, Theme } from '@emotion/react';
 import { flexGenerator } from '@styles/generator';
 
-export const inputWrapperStyle = css`
-  ${flexGenerator('row', 'first', 'center')}
-  position: relative;
+export const inputContainerStyle = css`
+  ${flexGenerator('column', 'center', 'flex-start')}
+  gap: 0.5rem;
   margin: 0 2rem;
 `;
 
-export const inputStyle = (theme: Theme) => css`
+export const inputWrapperStyle = css`
+  ${flexGenerator('row', 'first', 'center')}
+  position: relative;
+  width: 100%;
+`;
+
+export const inputStyle = (theme: Theme, isError: boolean) => css`
   min-width: 33.5rem;
   width: 100%;
   height: 5.2rem;
   padding: 1.5rem;
-  border: 1px solid ${theme.color.lightgray1};
+  border: 1px solid ${isError ? theme.color.error : theme.color.lightgray1};
   border-radius: 10px;
 
-  color: ${theme.color.darkgray};
+  color: ${isError ? theme.color.error : theme.color.darkgray};
   background-color: ${theme.color.white};
   ${theme.font['body01-r-15']}
 
@@ -28,9 +34,14 @@ export const inputStyle = (theme: Theme) => css`
   }
 `;
 
-export const textLengthStyle = (theme: Theme) => css`
+export const textLengthStyle = (theme: Theme, isError: boolean) => css`
   position: absolute;
   right: 1.5rem;
-  color: ${theme.color.midgray1};
+  color: ${isError ? theme.color.error : theme.color.midgray1};
   ${theme.font['body02-r-14']}
+`;
+
+export const errorMessageStyle = (theme: Theme) => css`
+  color: ${theme.color.error};
+  ${theme.font['body05-r-10']};
 `;
--- src/components/common/inputs/Input/Input.tsx
+++ src/components/common/inputs/Input/Input.tsx
@@ -1,42 +1,57 @@
 import { HTMLAttributes, useState } from 'react';
 import {
+  inputContainerStyle,
   inputWrapperStyle,
   inputStyle,
   textLengthStyle,
+  errorMessageStyle,
 } from 'src/components/common/inputs/Input/Input.style';
+import { useTheme } from '@emotion/react';
 
 export interface InputProps extends HTMLAttributes<HTMLInputElement> {
   placeholder: string;
   errorMessage: string;
   maxTextLength: number;
+  error: boolean;
 }
 
-const Input = ({ placeholder, errorMessage, maxTextLength, ...props }: InputProps) => {
+const Input = ({ placeholder, errorMessage, maxTextLength, error, ...props }: InputProps) => {
   const [value, setValue] = useState('');
+  const [isError, setIsError] = useState(false);
 
+  // 글자 수 세서 바로 화면에 반영하는 onChange 함수
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
+    setValue(e.target.value.slice(0, maxTextLength)); // 최대 길이 넘어가면 잘리게.
+
+    if (error) {
+      setIsError(true);
+    } else {
+      setIsError(false);
+    }
   };
 
+  // 글자 수 세는 함수
   const getTextLength = () => {
     return value.length;
   };
 
+  const theme = useTheme();
+
   return (
-    <div>
+    <div css={inputContainerStyle} {...props}>
       <div css={inputWrapperStyle}>
         <input
-          css={inputStyle}
+          css={[inputStyle(theme, isError)]}
           placeholder={placeholder}
           value={value}
           onChange={handleInputChange}
         />
-        <span css={textLengthStyle}>
+        <span css={textLengthStyle(theme, isError)}>
           {getTextLength()}/{maxTextLength}
         </span>
       </div>
 
-      <span>{errorMessage}</span>
+      {isError ? <span css={errorMessageStyle}>{errorMessage}</span> : null}
     </div>
   );
 };
