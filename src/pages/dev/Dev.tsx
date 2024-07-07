af56ebbcf9e5ff3476103a7b74e41ea092eeab57
imhwarang <imhwarang4@naver.com>
2024-07-07 23:33:55 +0900

    feat: ClipboardCopyButton 컴포넌트 완성

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,7 +1,13 @@
 import { ProgressBar } from '@components';
+import ClipboardCopyButton from 'src/components/common/buttons/ClipboardCopyButton/ClipboardCopyButton';
 
 const Dev = () => {
-  return <></>;
+  const text = '12313413413';
+  return (
+    <>
+      <ClipboardCopyButton text={text} />
+    </>
+  );
 };
 
 export default Dev;
