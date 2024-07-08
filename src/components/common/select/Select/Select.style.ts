576b5767905b5b7ef6a9c00568768a0acbb4ec61
장정안 <wjddks96@naver.com>
2024-07-08 18:29:08 +0900

    feat: Select 컴포넌트 구현

---
+++ src/components/common/select/Select/Select.style.ts
@@ -0,0 +1,50 @@
+import { Theme } from '@emotion/react';
+import { css } from '@emotion/react';
+import { flexGenerator } from '@styles/generator';
+
+export const selectContainer = (theme: Theme) => css`
+  ${flexGenerator('row', 'space-between', 'center')};
+  position: relative;
+  width: 8.7rem;
+  height: 2.8rem;
+  padding: 0.8rem;
+
+  color: ${theme.color.darkgray};
+  ${theme.font['body04-m-12']};
+
+  border-radius: 5px;
+  border: 1px solid ${theme.color.lightgray1};
+  background: ${theme.color.white};
+`;
+
+export const iconStyle = css`
+  width: 2.4rem;
+  height: 2.4rem;
+`;
+
+export const sortList = (theme: Theme) => css`
+  ${flexGenerator('column', 'center', 'flex-start')};
+  position: absolute;
+  top: 3.6rem;
+  width: 11.5rem;
+  height: 17.6rem;
+  padding: 0.4rem;
+
+  border-radius: 5px;
+  background: ${theme.color.white};
+
+  box-shadow: 0px 5px 20px 0px rgba(0, 0, 0, 0.15);
+`;
+
+export const sortItem = (theme: Theme) => css`
+  ${flexGenerator('row', 'flex-start', 'center')}
+  width: 100%;
+  height: 2.8rem;
+  gap: 0.2rem;
+
+  cursor: pointer;
+
+  &:hover {
+    ${theme.color.lightgray1}
+  }
+`;
---
+++ src/components/common/select/Select/Select.tsx
@@ -0,0 +1,42 @@
+import { HTMLAttributes, useState } from 'react';
+import { iconStyle, selectContainer, sortItem, sortList } from './Select.style';
+
+interface SelectProps extends HTMLAttributes<HTMLDivElement> {
+  title?: string;
+  icon: React.ReactNode;
+}
+
+const options = ['전체', '입금 대기', '승인 대기', '승인 완료', '승인 거절', '환불 완료'];
+
+const Select = ({ icon }: SelectProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+    console.log('클릭!!');
+  };
+
+  return (
+    <div css={selectContainer} onClick={toggleDropdown}>
+      {selectedOption}
+      <span css={iconStyle}>{icon}</span>
+      {isOpen && (
+        <ul css={sortList}>
+          {options.map((opt, i) => (
+            <li css={sortItem} key={i} onClick={() => handleOptionClick(opt)}>
+              {opt}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Select;
