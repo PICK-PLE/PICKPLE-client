1e631f3f85e8d680154c7c555d0c2c14264623f5
Parkchaeyeon <ccyy1029@naver.com>
2024-07-09 17:30:02 +0900

    feat: Dev 파일에 추가해두기

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,4 +1,4 @@
-import { ProgressBar } from '@components';
+import { Notice, ProgressBar } from '@components';
 import LogoHeader from 'src/components/common/headers/LogoHeader/LogoHeader';
 import { devContainer } from './Dev.style';
 
@@ -9,6 +9,10 @@ const Dev = () => {
       <ProgressBar progress={77} />
       <section css={devContainer}>
         {/* 여기에 컴포넌트 추가해보고, 디바이스 크기 조정해보면서 테스트 */}
+        <Notice numberLabel="1">
+          모임 참여 전, 호스트가 게시하는 공지사항을 <br />
+          꼼꼼히 확인해주세요.
+        </Notice>
       </section>
     </>
   );
