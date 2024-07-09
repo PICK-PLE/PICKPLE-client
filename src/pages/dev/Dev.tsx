c5b9dc8fa00899a1ef9eefc9800ae76bb336aea3
장정안 <wjddks96@naver.com>
2024-07-10 02:39:57 +0900

    feat: Dev 페이지 내 Image 컴포넌트 추가

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,6 +1,8 @@
 import { ProgressBar } from '@components';
 import LogoHeader from 'src/components/common/headers/LogoHeader/LogoHeader';
 import { devContainer } from './Dev.style';
+import Image from 'src/components/common/Image/Image';
+import Label from 'src/components/common/Label/Label';
 
 const Dev = () => {
   return (
@@ -9,6 +11,19 @@ const Dev = () => {
       <ProgressBar progress={77} />
       <section css={devContainer}>
         {/* 여기에 컴포넌트 추가해보고, 디바이스 크기 조정해보면서 테스트 */}
+        <Image
+          variant="square"
+          width="200px"
+          height="200px"
+          label={<Label variant="status">마감</Label>}
+        />
+
+        <Image
+          variant="square"
+          width="200px"
+          height="200px"
+          label={<Label variant="status">입금 대기</Label>}
+        />
       </section>
     </>
   );
