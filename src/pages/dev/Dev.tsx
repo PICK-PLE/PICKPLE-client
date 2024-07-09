321dfa88f7845c6909085622ceb9ae588bd170c9
장정안 <wjddks96@naver.com>
2024-07-09 17:33:20 +0900

    feat: Dev 페이지 내 TimeSelect 삽입

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,6 +1,7 @@
 import { ProgressBar } from '@components';
 import LogoHeader from 'src/components/common/headers/LogoHeader/LogoHeader';
 import { devContainer } from './Dev.style';
+import TimeSelect from 'src/components/common/TimeSelect/TimeSelect';
 
 const Dev = () => {
   return (
@@ -9,6 +10,7 @@ const Dev = () => {
       <ProgressBar progress={77} />
       <section css={devContainer}>
         {/* 여기에 컴포넌트 추가해보고, 디바이스 크기 조정해보면서 테스트 */}
+        <TimeSelect />
       </section>
     </>
   );
