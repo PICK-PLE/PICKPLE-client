61046514f2f8674c7cd15a95e148b2dba5d7a2cb
imhwarang <imhwarang4@naver.com>
2024-07-10 00:49:30 +0900

    refactor: 코드리뷰 반영

--- src/assets/svg/IcDefaultUserimg.tsx
+++ src/assets/svg/IcDefaultUserimg.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgIcDefaultUserimg = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 42 42" {...props}>
