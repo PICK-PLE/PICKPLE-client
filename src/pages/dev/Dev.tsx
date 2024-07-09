a71d36e8145513ce64d050f8fef6710b78ccab07
imhwarang <imhwarang4@naver.com>
2024-07-09 18:33:01 +0900

    refactor: Dev 컴포넌트에 CountPeople 컴포넌트 호출

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,7 +1,16 @@
-import { ProgressBar } from '@components';
+import { CountPeople } from '@components';
+import { useState } from 'react';
 
 const Dev = () => {
-  return <></>;
+  const [people, setPeople] = useState(7);
+  const handlePeopleChange = (newCount: number) => {
+    setPeople(newCount);
+  };
+  return (
+    <>
+      <CountPeople people={people} onPeopleChange={handlePeopleChange} />
+    </>
+  );
 };
 
 export default Dev;
