e782591abe40751b7020ff80c5ded35ba6c7cbdd
Yoo TaeSeung <remicon99@gmail.com>
2024-07-10 04:42:36 +0900

    refactor: dev페이지 수정

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,7 +1,6 @@
-import { NavigateBox, ProgressBar } from '@components';
+import { ProgressBar } from '@components';
 import LogoHeader from 'src/components/common/headers/LogoHeader/LogoHeader';
 import { devContainer } from './Dev.style';
-import Select from 'src/components/common/select/Select';
 
 const Dev = () => {
   return (
@@ -10,7 +9,6 @@ const Dev = () => {
       <ProgressBar progress={77} />
       <section css={devContainer}>
         {/* 여기에 컴포넌트 추가해보고, 디바이스 크기 조정해보면서 테스트 */}
-        <NavigateBox path={"/"}>로그아웃</NavigateBox>
       </section>
     </>
   );
