c2d268f6c6e09320e19d5b6bd150eba87a870efb
장정안 <wjddks96@naver.com>
2024-07-09 18:41:37 +0900

    feat: Select 컴포넌트에 필요한  svg팡리 추가

---
+++ src/assets/svg/IcDropUp.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgIcDropUp = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      stroke="#222224"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="m7 9 5.049 5L17 9"
+    />
+  </svg>
+);
+export default SvgIcDropUp;
