66e89e3371f6ce24b797695b0e2c09ff0bc23a56
장정안 <wjddks96@naver.com>
2024-07-10 03:04:04 +0900

    feat: Dev 페이지 내 FilterSelect 삽입

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -3,9 +3,21 @@ import FilterSelect from 'src/components/common/select/FilterSelect/FilterSelect
 const Dev = () => {
   return (
     <>
-      <FilterSelect
-        options={['전체', '입금 대기', '승인 대기', '승인 완료', '승인 거절', '환불 완료']}
-      />
+      <div css={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <FilterSelect
+          options={['전체', '입금 대기', '승인 대기', '승인 완료', '승인 거절', '환불 완료']}
+        />
+      </div>
+      <div css={{ display: 'flex', justifyContent: 'center' }}>
+        <FilterSelect
+          options={['전체', '입금 대기', '승인 대기', '승인 완료', '승인 거절', '환불 완료']}
+        />
+      </div>
+      <div>
+        <FilterSelect
+          options={['전체', '입금 대기', '승인 대기', '승인 완료', '승인 거절', '환불 완료']}
+        />
+      </div>
     </>
   );
 };
