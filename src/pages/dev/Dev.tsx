9363c72aa6f7544df2b7997ad562b1d52f989f51
장정안 <wjddks96@naver.com>
2024-07-09 23:36:08 +0900

    feat: Dev 페이지에 Image 컴포넌트 추가

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,10 +1,12 @@
 import { Image } from '@components';
+import { IcCopyPlus } from '@svg';
 
 const Dev = () => {
   return (
-    <>
+    <div css={{ display: 'flex', gap: '2rem' }}>
+      <Image variant="square" width="200px" height="200px" label={<IcCopyPlus />}></Image>
       <Image variant="square" width="200px" height="200px"></Image>
-    </>
+    </div>
   );
 };
 
