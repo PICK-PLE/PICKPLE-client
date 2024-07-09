b9ee3fbbfaf3d634880472660e8377e871ba173a
Parkchaeyeon <ccyy1029@naver.com>
2024-07-09 17:25:53 +0900

    feat: margin 삭제하고 justify-content 수정

--- src/components/common/QuestionText/QuestionText.style.ts
+++ src/components/common/QuestionText/QuestionText.style.ts
@@ -2,13 +2,10 @@ import { css, Theme } from '@emotion/react';
 import { flexGenerator } from '@styles/generator';
 
 export const questionStyle = (theme: Theme) => css`
-  ${flexGenerator('row', 'first', 'center')}
+  ${flexGenerator('row', 'flex-start', 'center')}
   gap: 1rem;
-  margin: 0 2rem;
   padding: 1rem;
-
   background-color: ${theme.color.background};
-
   border-radius: 10px;
 `;
 
--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -1,4 +1,4 @@
-import { ProgressBar } from '@components';
+import { ProgressBar, QuestionText } from '@components';
 import LogoHeader from 'src/components/common/headers/LogoHeader/LogoHeader';
 import { devContainer } from './Dev.style';
 
@@ -9,6 +9,7 @@ const Dev = () => {
       <ProgressBar progress={77} />
       <section css={devContainer}>
         {/* 여기에 컴포넌트 추가해보고, 디바이스 크기 조정해보면서 테스트 */}
+        <QuestionText numberLabel="Q1">호스트님은 어떤 분이신가요?</QuestionText>
       </section>
     </>
   );
