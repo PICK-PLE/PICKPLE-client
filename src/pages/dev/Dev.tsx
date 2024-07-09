a4dc29bd4a801d581089fba42cbb1a9f470e1305
Parkchaeyeon <ccyy1029@naver.com>
2024-07-09 18:21:13 +0900

    feat: justifycontent 수정, dev 수정

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -13,6 +13,16 @@ const Dev = () => {
           모임 참여 전, 호스트가 게시하는 공지사항을 <br />
           꼼꼼히 확인해주세요.
         </Notice>
+
+        <Notice numberLabel="2">
+          무단으로 불참하거나, 함께하는 이들에게 <br />
+          피해를 주는 경우 이용 제재를 받게 돼요.
+        </Notice>
+
+        <Notice numberLabel="3">
+          서로에게 유익한 시간이 되도록 <br />
+          열정적인 태도로 임해주세요.
+        </Notice>
       </section>
     </>
   );
