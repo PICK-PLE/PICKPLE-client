2615cd66d94de67e16ed9a623202a242d55ed591
chaeneey <127743435+chaeneey@users.noreply.github.com>
2024-07-10 01:32:48 +0900

    [Feat/#49] QuestionInput 컴포넌트 구현 (#51)
    * feat: NumberLabel 컴포넌트 만들기
    
    * chore: 필요없는 코드 삭제
    
    * style: fontWeight 제거
    
    * chore: index.ts 에 NumberLabel 추가
    
    * feat: Notice 컴포넌트 만들기
    
    * style: flexGenerator 사용하기
    
    * feat: QuestionText 만들기
    
    * style: 글씨색, 배경색 변경
    
    * chore: dev 페이지 내용 지우기
    
    * fix: children 속성 빼기
    
    * chore: index.ts 에 QuestionText 추가
    
    * feat: QuestionInput 구현
    
    * fix: NumberLabel 컴포넌트 div로 감싸서 css 스타일 주기
    
    * style: width 를 100%로 변경하기
    
    * feat: 외부에서 value와 onChange 받아오기!
    
    * feat: margint 값 지우고 maxTextLength 에 따라서 글자수 입력 제한
    
    * feat: justifycontent 수정
    
    * chore: 안쓰는 console 지우기
    
    * refactor: QuestionInput 스타일 파일 수정
    
    * feat: InputHTMLAttributes 사용해서 필요없는 것들 빼기!
    
    * refactor: 필요없는 속성 삭제
    
    ---------
    
    Co-authored-by: imhwarang <imhwarang4@naver.com>
    Co-authored-by: Yoo TaeSeung <remicon99@gmail.com>

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -6,10 +6,23 @@ import NumberLabel from 'src/components/common/NumberLabel/NumberLabel';
 import ProgressBar from './ProgressBar/ProgressBar';
 import Notice from 'src/components/common/Notice/Notice';
 import QuestionText from 'src/components/common/QuestionText/QuestionText';
+import QuestionInput from 'src/components/common/QuestionInput/QuestionInput';
 import SelectButton from './buttons/SelectButton/SelectButton';
 import Label from './Label/Label';
 import CategoryButton from './buttons/CategoryButton/CategoryButton';
 import InterestCategoryButton from './buttons/InterestCategoryButton/InterestCategoryButton';
 import ShareButton from './buttons/ShareButton/ShareButton';
 
-export { IconButton, SocialLoginButton, PayButton, Header, NumberLabel, ProgressBar, SelectButton, Label, Notice, QuestionText, CategoryButton, InterestCategoryButton, ShareButton };
+export {
+  IconButton,
+  SocialLoginButton,
+  PayButton,
+  Header,
+  NumberLabel,
+  ProgressBar,
+  Notice,
+  QuestionText,
+  QuestionInput,
+  SelectButton, 
+  Label
+};
