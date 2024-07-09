ab8ff36e3f92f832cfc98154d28ddfc6782fcad1
장정안 <126966681+ExceptAnyone@users.noreply.github.com>
2024-07-10 04:16:38 +0900

    [Feat/#73] TimeSelect 컴포넌트 구현 (#74)
    * feat: TimeSelect 컴포넌트 구현
    
    * feat: ic_down svg 파일 추가
    
    * feat: 기본 select 태그 내 아이콘 안보이게 설정
    
    * feat: TimeSelect 컴포넌트 내 ref로 아이콘 클릭 시에도 기능 동작하게 구현
    
    * feat: Dev 페이지 내 TimeSelect 삽입
    
    * feat: Theme, css 합치기
    
    * feat: width 100%로 변경 & appearance: none 추가
    
    * feat: 함수 네이밍 generateTimeOptions로 변경
    
    * feat: 주석 제거
    
    * feat: 종료시간 먼저 선택시 showPicker 오류 뜨는 현상 해결
    
    ---------
    
    Co-authored-by: Yoo TaeSeung <remicon99@gmail.com>

---
+++ src/components/common/TimeSelect/TimeSelect.style.ts
@@ -0,0 +1,48 @@
+import { Theme, css } from '@emotion/react';
+import { flexGenerator } from '@styles/generator';
+
+export const timeSelectContainer = css`
+  ${flexGenerator()}
+  gap: 1.1rem;
+`;
+
+export const timeSelectWrapper = (theme: Theme) => css`
+  ${flexGenerator('column', 'center', 'flex-start')};
+  width: 100%;
+  padding: 1rem 1.6rem;
+  gap: 0.3rem;
+  border: 1px solid ${theme.color.lightgray1};
+  border-radius: 10px;
+`;
+
+export const selectStyle = (theme: Theme) => css`
+  width: 13rem;
+  border: none;
+  border-radius: 4px;
+  color: ${theme.color.midgray1};
+  ${theme.font['body01-r-15']};
+
+  cursor: pointer;
+
+  appearance: none;
+  -webkit-appearance: none;
+`;
+
+export const textStyle = (theme: Theme) => css`
+  color: ${theme.color.midgray1};
+  ${theme.font['body03-r-12']};
+`;
+
+export const iconStyle = css`
+  ${flexGenerator()}
+  width: 2.4rem;
+  height: 2.4rem;
+  right: 0;
+  position: absolute;
+
+  cursor: pointer;
+`;
+
+export const labelWrapper = css`
+  position: relative;
+`;
