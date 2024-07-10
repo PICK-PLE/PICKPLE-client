bd3383fe46fd11644e52be11e50d1c8c6a91e702
imhwarang <imhwarang4@naver.com>
2024-07-11 01:03:39 +0900

    feat: ApplicantListModal 컴포넌트 틀 작업

---
+++ src/pages/host/components/ApplicantListModal/ApplicantListModal.style.ts
@@ -0,0 +1,5 @@
+import { css } from "@emotion/react";
+
+export const modalContainerStyle = css`
+  padding: 4rem 1.7rem 1.7rem 1.7rem;
+`;
---
+++ src/pages/host/components/ApplicantListModal/ApplicantListModal.tsx
@@ -0,0 +1,16 @@
+import { Modal } from '@components'
+import React from 'react'
+
+const ApplicantListModal = () => {
+  return (
+    <article>
+        <section>
+            <h1>아래 계좌로 입금해주세요!</h1>
+        </section>
+        <section></section>
+        <section></section>
+    </article>
+  )
+}
+
+export default ApplicantListModal
