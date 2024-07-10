39286309531b540c40a97f19827957f710f53ef8
thisishwarang <101498590+thisishwarang@users.noreply.github.com>
2024-07-11 01:06:39 +0900

    [Feat/#82] GuestClassRegisterCard 컴포넌트 완료 (#86)
    * feat: 컴포넌트 기본 틀 작성
    
    * feat: GuestClassRegisterCard 컴포넌트 스타일 작성중
    
    * refactor: Dev 컴포넌트 원래대로 수정
    
    * refactor: Dev 컴포넌트 원래대로 수정
    
    * refactor: 코드리뷰 반영

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -2,6 +2,7 @@ import { Carousel, ProgressBar } from '@components';
 import LogoHeader from 'src/components/common/headers/LogoHeader/LogoHeader';
 import { devContainer } from './Dev.style';
 import ApplicantAccordionList from 'src/components/common/applicantAccordions/ApplicantAccoridonList/ApplicantAccordionList';
+import GuestClassRegisterCard from '../class/components/GuestClassRegisterCard/GuestClassRegisterCard';
 import SimpleUserProfile from 'src/components/common/SimpleUserProfile/SimpleUserProfile';
 
 const Dev = () => {
@@ -12,18 +13,22 @@ const Dev = () => {
     'https://placehold.co/600x400',
     'https://placehold.co/600x400',
   ];
+
   return (
     <>
       <LogoHeader />
       <ProgressBar progress={77} />
       <Carousel imageList={imageList} />
       <section css={devContainer}>
-        {/* 여기에 컴포넌트 추가해보고, 디바이스 크기 조정해보면서 테스트 */}
-        <ApplicantAccordionList moimId={1} />
+      {/* 여기에 컴포넌트 추가해보고, 디바이스 크기 조정해보면서 테스트 */}
+      <ApplicantAccordionList moimId={1} />
       </section>
       <SimpleUserProfile size="large" username="신청자" />
       <SimpleUserProfile size="medium" username="달아오르구마" />
       <SimpleUserProfile size="small" username="달아아오르구마마마마" />
+      <div css={devContainer}>
+        <GuestClassRegisterCard />
+      </div>
     </>
   );
 };
