52c47f3c1dd5e0d674595739288de73cfbfbda37
imhwarang <imhwarang4@naver.com>
2024-07-11 05:34:10 +0900

    feat: checkmodal svg 가져옴

---
+++ src/assets/svg/IcCheckModal.tsx
@@ -0,0 +1,20 @@
+import type { SVGProps } from 'react';
+const SvgIcCheckModal = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 33 32" {...props}>
+    <path
+      stroke="#6360FF"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2.667}
+      d="M4.5 16c0-6.627 5.373-12 12-12 6.628 0 12 5.373 12 12 0 6.628-5.372 12-12 12-6.627 0-12-5.372-12-12"
+    />
+    <path
+      stroke="#6360FF"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2.667}
+      d="m11.167 16 4 4 6.666-6.666"
+    />
+  </svg>
+);
+export default SvgIcCheckModal;
