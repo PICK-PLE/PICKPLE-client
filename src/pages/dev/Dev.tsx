6a9deefcde99c84b1e02eed647019419b927b030
thisishwarang <101498590+thisishwarang@users.noreply.github.com>
2024-07-10 22:52:17 +0900

    [Feat/#84] SimpleUserProfile 컴포넌트 size별로 스타일 수정

--- src/pages/dev/Dev.tsx
+++ src/pages/dev/Dev.tsx
@@ -2,6 +2,7 @@ import { Carousel, ProgressBar } from '@components';
 import LogoHeader from 'src/components/common/headers/LogoHeader/LogoHeader';
 import { devContainer } from './Dev.style';
 import ApplicantAccordionList from 'src/components/common/applicantAccordions/ApplicantAccoridonList/ApplicantAccordionList';
+import SimpleUserProfile from 'src/components/common/SimpleUserProfile/SimpleUserProfile';
 
 const Dev = () => {
   const imageList = [
@@ -20,6 +21,9 @@ const Dev = () => {
         {/* 여기에 컴포넌트 추가해보고, 디바이스 크기 조정해보면서 테스트 */}
         <ApplicantAccordionList moimId={1} />
       </section>
+      <SimpleUserProfile size="large" username="신청자" />
+      <SimpleUserProfile size="medium" username="달아오르구마" />
+      <SimpleUserProfile size="small" username="달아아오르구마마마마" />
     </>
   );
 };
