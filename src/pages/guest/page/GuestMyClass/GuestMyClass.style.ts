e3071a9f36e53b51269ba60508ffe2036126ed16
장정안 <wjddks96@naver.com>
2024-07-12 01:26:37 +0900

    feat: GuestMyClass 신청한 탭 ui 구현

---
+++ src/pages/guest/page/GuestMyClass/GuestMyClass.style.ts
@@ -0,0 +1,64 @@
+import { Theme } from '@emotion/react';
+import { css } from '@emotion/react';
+import { flexGenerator } from '@styles/generator';
+
+export const guestMyClassLayout = css`
+  height: 4.4rem;
+  align-self: stretch;
+`;
+
+export const tapLine = (theme: Theme) => css`
+  width: 100%;
+  height: 0.1rem;
+
+  ${theme.color.lightgray1}
+`;
+
+export const tapContainer = (theme: Theme) => css`
+  ${flexGenerator('row', 'center', 'center')}
+  width: 50%;
+  height: 4.4rem;
+  padding: 0.8rem;
+  gap: 0.8rem;
+  flex-shrink: 0;
+
+  background: ${theme.color.white};
+
+  cursor: pointer;
+`;
+
+export const activeTabStyle = (theme: Theme) => css`
+  color: ${theme.color.purple1};
+  border-bottom: 0.1rem solid ${theme.color.purple1};
+`;
+
+export const inactiveTabStyle = (theme: Theme) => css`
+  color: ${theme.color.lightgray2};
+  border-bottom: 0.1rem solid ${theme.color.lightgray2};
+`;
+
+export const tabWrapper = css`
+  ${flexGenerator()}
+`;
+
+export const getTabStyle = (isActive: boolean) => (theme: Theme) =>
+  css`
+    ${tapContainer(theme)}
+    ${isActive ? activeTabStyle(theme) : inactiveTabStyle(theme)}
+    ${theme.font['subhead02-sb-16']}
+  `;
+
+export const filterSelectWrapper = (theme: Theme) => css`
+  ${flexGenerator('row', 'flex-end', 'flex-end')}
+  margin-right: 2rem;
+
+  background: ${theme.color.background};
+`;
+
+export const guestMyClassCardContainer = (theme: Theme) => css`
+  background-color: ${theme.color.background};
+`;
+
+export const filterSelectStyle = css`
+  margin-top: 1.8rem;
+`;
