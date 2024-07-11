5777d4858ced7f31be367ea626cbf3efe786f46a
장정안 <wjddks96@naver.com>
2024-07-11 22:21:02 +0900

    feat: 목데이터 추가로 제공

--- src/pages/guest/components/GuestClassAppliedCard/GuestClassAppliedCard.tsx
+++ src/pages/guest/components/GuestClassAppliedCard/GuestClassAppliedCard.tsx
@@ -17,6 +17,19 @@ const MOCK_DATA: { data: Moim[] } = {
       fee: 10000,
       hostNickname: '달아올랐구마',
     },
+    {
+      moimId: 1,
+      moimSubmissionState: 'REJECTED',
+      moimTitle: '부산10년 토박이 달아오르구마와 함께하는 사투리 모임',
+      dateList: {
+        date: '2024.07.15',
+        dayOfWeek: '월',
+        startTime: '10:00',
+        endTime: '12:00',
+      },
+      fee: 10000,
+      hostNickname: '달아올랐구마',
+    },
   ],
 };
 
