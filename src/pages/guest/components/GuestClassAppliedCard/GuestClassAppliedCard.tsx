7e585408428771463fbc3a64ceb6cae7a8e484f1
장정안 <wjddks96@naver.com>
2024-07-11 22:45:59 +0900

    feat: 모임상태 COMPLETED인 mock data 추가

--- src/pages/guest/components/GuestClassAppliedCard/GuestClassAppliedCard.tsx
+++ src/pages/guest/components/GuestClassAppliedCard/GuestClassAppliedCard.tsx
@@ -18,8 +18,21 @@ const MOCK_DATA: { data: Moim[] } = {
       hostNickname: '달아올랐구마',
     },
     {
-      moimId: 1,
+      moimId: 2,
       moimSubmissionState: 'REJECTED',
+      moimTitle: '부산10년 토박이 달아오르구마와 함께하는 사투리 모임',
+      dateList: {
+        date: '2024.07.15',
+        dayOfWeek: '월',
+        startTime: '10:00',
+        endTime: '12:00',
+      },
+      fee: 10000,
+      hostNickname: '달아올랐구마',
+    },
+    {
+      moimId: 3,
+      moimSubmissionState: 'COMPLETED',
       moimTitle: '부산10년 토박이 달아오르구마와 함께하는 사투리 모임',
       dateList: {
         date: '2024.07.15',
