4ba5cfe9e016fc7a69632f49c6e3cccaa64f35f8
Parkchaeyeon <ccyy1029@naver.com>
2024-07-12 04:38:59 +0900

    feat: MyClassManagePage 만들기

--- src/pages/host/page/MyClassManage/MyClassManage.tsx
+++ src/pages/host/page/MyClassManage/MyClassManage.tsx
@@ -1,5 +1,16 @@
+import { ApplicantAccordionList, Button, Header, Label } from "@components";
+
 const MyClassManage = () => {
-  return <div>MyClassManage</div>;
+  return(<div style={{margin: "0 2rem"}}>
+    <Header title="신청자 관리" />
+    <div>부산 10년 토박이 달아오르구마와 함께하는 사투리리</div>
+    <div>
+      <span>모임 신청자 (12)</span>
+      <Label variant="count">2</Label>
+    </div>
+    <ApplicantAccordionList moimId={1} />
+    <Button variant="large">승인하기</Button>
+  </div>);
 };
 
 export default MyClassManage;
