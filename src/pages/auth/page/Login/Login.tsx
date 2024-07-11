3e37ba7307c27e44e14854fd3873faec0674ce29
Yoo TaeSeung <remicon99@gmail.com>
2024-07-12 02:32:56 +0900

    feat: 로그인 페이지 퍼블리싱

--- src/pages/auth/page/Login/Login.tsx
+++ src/pages/auth/page/Login/Login.tsx
@@ -1,5 +1,16 @@
+import { SocialLoginButton } from '@components';
+import { IcKakaoLogo, Logo } from '@svg';
+import { loginWrapper, logoStyle } from './Login.style';
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div css={loginWrapper}>
+      <Logo css={logoStyle} />
+      <SocialLoginButton platform="kakao" icon={<IcKakaoLogo />}>
+        카카오로 시작하기
+      </SocialLoginButton>
+    </div>
+  );
 };
 
 export default Login;
