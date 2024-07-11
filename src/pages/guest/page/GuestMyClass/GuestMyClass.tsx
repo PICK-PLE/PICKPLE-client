7734d579d2d3174ae02a724caedcdb3e23129ded
장정안 <wjddks96@naver.com>
2024-07-12 00:30:24 +0900

    feat: 페이지 내 Tab 구현

--- src/pages/guest/page/GuestMyClass/GuestMyClass.tsx
+++ src/pages/guest/page/GuestMyClass/GuestMyClass.tsx
@@ -1,10 +1,38 @@
+import { FilterSelect, Header } from '@components';
 import GuestMyClassCard from '@pages/guest/components/GuestMyClassCard/GuestMyClassCard';
+import {
+  guestMyClassLayout,
+  tapLine,
+  tabWrapper,
+  getTabStyle,
+  filterSelectContainer,
+} from './GuestMyClass.style';
+import { useState } from 'react';
 
 const GuestMyClass = () => {
+  const [activeTab, setActiveTab] = useState<'신청한' | '참가한'>('신청한');
+
   return (
-    <div>
+    <>
+      <Header title="my 클래스 모임" onClick={() => {}} />
+      <article css={guestMyClassLayout}>
+        <div css={tapLine} />
+        <div css={tabWrapper}>
+          <div css={getTabStyle(activeTab === '신청한')} onClick={() => setActiveTab('신청한')}>
+            신청한
+          </div>
+          <div css={getTabStyle(activeTab === '참가한')} onClick={() => setActiveTab('참가한')}>
+            참가한
+          </div>
+        </div>
+      </article>
+      <article css={filterSelectContainer}>
+        <FilterSelect
+          options={['전체', '입금 대기', '승인 대기', '승인 완료', '승인 거절', '환불 완료']}
+        />
+      </article>
       <GuestMyClassCard />
-    </div>
+    </>
   );
 };
 
