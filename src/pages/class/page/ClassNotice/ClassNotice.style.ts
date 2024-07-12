971c1a3c8958d39bf1604b6687ca7c2ba5745b18
Yoo TaeSeung <remicon99@gmail.com>
2024-07-12 18:25:07 +0900

    feat: 공지사항 작성 페이지 퍼블리싱

---
+++ src/pages/class/page/ClassNotice/ClassNotice.style.ts
@@ -0,0 +1,26 @@
+import { css, Theme } from '@emotion/react';
+import { flexGenerator } from '@styles/generator';
+
+export const noticePostBackground = (theme: Theme) => css`
+  background-color: ${theme.color.background};
+
+  min-height: 100dvh;
+`;
+
+export const noticePostLayout = css`
+  width: 100%;
+  height: calc(100dvh - 6.3rem);
+  ${flexGenerator('column', 'space-between')};
+
+  padding: 3.7rem 2rem 3rem;
+`;
+
+export const noticePostMain = css`
+  width: 100%;
+  ${flexGenerator('column', 'flex-start', 'flex-start')};
+  gap: 1rem;
+`;
+
+export const imageSelectWrapper = css`
+  margin-top: 2rem;
+`;
