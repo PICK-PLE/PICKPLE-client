a4a5ef53df942f0f3c1ad6d6352fca2f3ce9d499
장정안 <wjddks96@naver.com>
2024-07-12 21:36:20 +0900

    feat: GuestMyPage UI 구현

---
+++ src/pages/myPage/page/GuestMyPage/GuestMyPage.style.ts
@@ -0,0 +1,48 @@
+import { Theme, css } from '@emotion/react';
+import { flexGenerator } from '@styles/generator';
+
+export const GuestMyPageLayout = css`
+  ${flexGenerator('column', 'flex-start', 'flex-start')};
+  gap: 2.8rem;
+`;
+
+export const userDistinguishWrapper = css`
+  ${flexGenerator('row', 'flex-start', 'flex-start')};
+  padding-left: 2rem;
+  gap: 2rem;
+`;
+
+export const userSelectedTextStyle = (theme: Theme) => css`
+  color: ${theme.color.black};
+  ${theme.font['head03-b-18']}
+
+  cursor: pointer;
+`;
+
+export const userUnselectedTextStyle = (theme: Theme) => css`
+  color: ${theme.color.lightgray2};
+  ${theme.font['head03-b-18']}
+
+  cursor: pointer;
+`;
+
+export const simpleUserProfileWrapper = css`
+  margin-top: 3rem;
+  padding-left: 2rem;
+`;
+
+export const navigateBoxWrapper = css`
+  ${flexGenerator('column', 'flex-start', 'flex-start')}
+  width: 100%;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  gap: 2.8rem;
+`;
+
+export const line = (theme: Theme) => css`
+  ${flexGenerator('row', 'center', 'stretch')};
+  width: 100%;
+  height: 0.8rem;
+
+  background-color: ${theme.color.background};
+`;
--- src/pages/myPage/page/GuestMyPage/GuestMyPage.tsx
+++ src/pages/myPage/page/GuestMyPage/GuestMyPage.tsx
@@ -1,5 +1,36 @@
+import { LogoHeader, NavigateBox, SimpleUserProfile } from '@components';
+import {
+  GuestMyPageLayout,
+  navigateBoxWrapper,
+  simpleUserProfileWrapper,
+  userDistinguishWrapper,
+  userSelectedTextStyle,
+  userUnselectedTextStyle,
+  line,
+} from './GuestMyPage.style';
+import { routePath } from '@constants';
+
 const GuestMyPage = () => {
-  return <div>GuestMyPage</div>;
+  return (
+    <>
+      <LogoHeader />
+      <div css={GuestMyPageLayout}>
+        <div css={userDistinguishWrapper}>
+          <p css={userSelectedTextStyle}>게스트</p>
+          <p css={userUnselectedTextStyle}>호스트</p>
+        </div>
+        <article css={simpleUserProfileWrapper}>
+          <SimpleUserProfile size="large" username="신청자" />
+        </article>
+        <div css={line} />
+        <article css={navigateBoxWrapper}>
+          <NavigateBox path={routePath.GUEST_MY_CLASS}>my 클래스 모임</NavigateBox>
+          <NavigateBox path="오픈 카톡 링크">픽플에 문의하기</NavigateBox>
+          <NavigateBox path="로그아웃 로직">로그아웃</NavigateBox>
+        </article>
+      </div>
+    </>
+  );
 };
 
 export default GuestMyPage;
