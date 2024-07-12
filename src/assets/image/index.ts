6e325a471d24688c452c5f96fed085e6f704f686
imhwarang <imhwarang4@naver.com>
2024-07-13 00:05:31 +0900

    feat: ClassPostPage 뷰 완성

---
+++ src/assets/image/index.ts
@@ -0,0 +1,3 @@
+import ClassPostFinishPng from './image_class_post_finish.png';
+
+export { ClassPostFinishPng };
--- src/pages/class/components/StepFour/StepFour.tsx
+++ src/pages/class/components/StepFour/StepFour.tsx
@@ -1,5 +1,39 @@
+import { Button, ProgressBar } from '@components';
+import {
+  footerStyle,
+  headerSpanStyle,
+  headerStyle,
+  imageStyle,
+  layoutStyle,
+  mainStyle,
+  subTitleStyle,
+  titleStyle,
+} from './StepFour.style';
+import { ClassPostFinishPng } from '@image';
+
 const StepFour = () => {
-  return <div>StepFour</div>;
+  return (
+    <>
+      <ProgressBar progress={100} />
+      <div css={layoutStyle}>
+        <header css={headerStyle}>
+          <h4 css={titleStyle}>클래스 모임 개설</h4>
+          <h1 css={subTitleStyle}>질문을 만들어 주세요</h1>
+          <span css={headerSpanStyle}>
+            [마이페이지] &gt; [my 클래스 모임]에서
+            <br />
+            개설한 모임을 관리할 수 있어요.
+          </span>
+        </header>
+        <main css={mainStyle}>
+          <img src={ClassPostFinishPng} css={imageStyle} />
+        </main>
+        <footer css={footerStyle}>
+          <Button variant="large">개설한 모임 보러가기</Button>
+        </footer>
+      </div>
+    </>
+  );
 };
 
 export default StepFour;
