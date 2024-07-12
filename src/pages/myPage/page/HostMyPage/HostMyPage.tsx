83a9a6e1b9949240fec040e4ce81b0939ebc681c
장정안 <wjddks96@naver.com>
2024-07-13 02:10:38 +0900

    feat: Host My Page  UI 구현

--- src/pages/myPage/page/HostMyPage/HostMyPage.tsx
+++ src/pages/myPage/page/HostMyPage/HostMyPage.tsx
@@ -1,5 +1,30 @@
+import { LogoHeader } from '@components';
+import {
+  userDistinguishWrapper,
+  userSelectedTextStyle,
+  userUnselectedTextStyle,
+} from './HostMyPage.style';
+import { useEasyNavigate } from '@hooks';
+import HostInfoCardWithLink from '@pages/myPage/components/HostInfoCardWithLink/HostInfoCardWithLink';
+
 const HostMyPage = () => {
-  return <div>HostMyPage</div>;
+  const { goGuestMyPage, goHostMyPage } = useEasyNavigate();
+  return (
+    <>
+      <LogoHeader />
+      <div>
+        <div css={userDistinguishWrapper}>
+          <p css={userUnselectedTextStyle} onClick={goGuestMyPage}>
+            게스트
+          </p>
+          <p css={userSelectedTextStyle} onClick={goHostMyPage}>
+            호스트
+          </p>
+        </div>
+      </div>
+      <HostInfoCardWithLink />
+    </>
+  );
 };
 
 export default HostMyPage;
