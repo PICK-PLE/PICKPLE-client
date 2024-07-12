6dc849a8b6d7deb481431ec758f15a665515f722
장정안 <wjddks96@naver.com>
2024-07-12 15:34:04 +0900

    feat: Card.tsx 내 {} 추가

--- src/pages/dev/page/Card/Card.tsx
+++ src/pages/dev/page/Card/Card.tsx
@@ -4,9 +4,11 @@ import { CLASSLIST_DATA } from 'src/constants/mocks/classListCardData';
 
 const Card = () => {
   return (
-    <div style={{margin: '0 2rem' }}>
+    <div style={{ margin: '0 2rem' }}>
       <HostInfoCard />
-      CLASSLIST_DATA.map((data) => <ClassListCard key={data.moimId} classListData={data} />);
+      {CLASSLIST_DATA.map((data) => (
+        <ClassListCard key={data.moimId} classListData={data} />
+      ))}
     </div>
   );
 };
