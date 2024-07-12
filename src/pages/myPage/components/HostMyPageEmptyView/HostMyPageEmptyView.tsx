d79d1a6db81cef20986f03bab990cf4fc72f0e1f
장정안 <wjddks96@naver.com>
2024-07-12 19:15:28 +0900

    feat: HostMyPageEmptyView ui 구현

---
+++ src/pages/myPage/components/HostMyPageEmptyView/HostMyPageEmptyView.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@components';
+import {
+  completedTabContainer,
+  detailWrapper,
+  textStyle,
+  textWrapper,
+} from './HostMyPageEmptyView.style';
+import { IcHostMypage } from '@svg';
+import { IcHostMypageStyle } from '@pages/myPage/page/HostMyPage/HostMyPage.style';
+
+interface HostMyPageEmptyViewProps {
+  text: string;
+}
+
+const HostMyPageEmptyView = ({ text }: HostMyPageEmptyViewProps) => {
+  return (
+    <article css={completedTabContainer}>
+      <IcHostMypage css={IcHostMypageStyle} />
+      <div css={detailWrapper}>
+        <div css={textWrapper}>
+          <p css={textStyle}>{text}</p>
+          <p css={textStyle}>다양한 클래스 모임을 둘러보세요:{')'}</p>
+        </div>
+        <Button variant="round">클래스 모임 개설하기</Button>
+      </div>
+    </article>
+  );
+};
+
+export default HostMyPageEmptyView;
