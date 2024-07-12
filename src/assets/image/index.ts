56aede9072fd9fe8aa7068f5266972590855e6b0
imhwarang <imhwarang4@naver.com>
2024-07-13 01:52:13 +0900

    feat: 카테고리 아이콘 전부 다운로드 및 index.ts, category.tsx에 설정 완료

---
+++ src/assets/image/index.ts
@@ -0,0 +1,83 @@
+import NJobFillSelectedImage from './fill-selected/image_njob_fill_selected.png';
+import NJobFillImage from './fill/image_njob_fill.png';
+import NJobStrokeSelectedImage from './stroke-selected/image_njob_stroke_selected.png';
+import NJobStrokeImage from './stroke/image_njob_stroke.png';
+import InvestmentFillSelectedImage from './fill-selected/image_investment_fill_selected.png';
+import InvestmentFillImage from './fill/image_investment_fill.png';
+import InvestmentStrokeSelectedImage from './stroke-selected/image_investment_stroke_selected.png';
+import InvestmentStrokeImage from './stroke/image_investment_stroke.png';
+import StartupFillSelectedImage from './fill-selected/image_startup_fill_selected.png';
+import StartupFillImage from './fill/image_startup_fill.png';
+import StartupStrokeSelectedImage from './stroke-selected/image_startup_stroke_selected.png';
+import StartupStrokeImage from './stroke/image_startup_stroke.png';
+import EmploymentFillSelectedImage from './fill-selected/image_employment_fill_selected.png';
+import EmploymentFillImage from './fill/image_employment_fill.png';
+import EmploymentStrokeSelectedImage from './stroke-selected/image_employment_stroke_selected.png';
+import EmploymentStrokeImage from './stroke/image_employment_stroke.png';
+import ProductivityFillSelectedImage from './fill-selected/image_productivity_fill_selected.png';
+import ProductivityFillImage from './fill/image_productivity_fill.png';
+import ProductivityStrokeSelectedImage from './stroke-selected/image_projuctivity_stroke_selected.png';
+import ProductivityStrokeImage from './stroke/image_productivity_stroke.png';
+import LifestyleFillSelectedImage from './fill-selected/image_lifestyle_fill_selected.png';
+import LifestyleFillImage from './fill/image_lifestyle_fill.png';
+import LifestyleStrokeSelectedImage from './stroke-selected/image_lifestyle_stroke_selected.png';
+import LifestyleStrokeImage from './stroke/image_lifestyle_stroke.png';
+import HealthFillSelectedImage from './fill-selected/image_health_fill_selected.png';
+import HealthFillImage from './fill/image_health_fill.png';
+import HealthStrokeSelectedImage from './stroke-selected/image_health_stroke_selected.png';
+import HealthStrokeImage from './stroke/image_health_stroke.png';
+import MindFillSelectedImage from './fill-selected/image_mind_fill_selected.png';
+import MindFillImage from './fill/image_mind_fill.png';
+import MindStrokeSelectedImage from './stroke-selected/image_mind_stroke_selected.png';
+import MindStrokeImage from './stroke/image_mind_stroke.png';
+import HobbyFillSelectedImage from './fill-selected/image_hobby_fill_selected.png';
+import HobbyFillImage from './fill/image_hobby_fill.png';
+import HobbyStrokeSelectedImage from './stroke-selected/image_hobby_stroke_selected.png';
+import HobbyStrokeImage from './stroke/image_hobby_stroke.png';
+import LanguageFillSelectedImage from './fill-selected/image_language_fill_selected.png';
+import LanguageFillImage from './fill/image_language_fill.png';
+import LanguageStrokeSelectedImage from './stroke-selected/image_language_stroke_selected.png';
+import LanguageStrokeImage from './stroke/image_language_stroke.png';
+
+export {
+  NJobFillSelectedImage,
+  NJobFillImage,
+  NJobStrokeSelectedImage,
+  NJobStrokeImage,
+  InvestmentFillSelectedImage,
+  InvestmentFillImage,
+  InvestmentStrokeSelectedImage,
+  InvestmentStrokeImage,
+  StartupFillSelectedImage,
+  StartupFillImage,
+  StartupStrokeSelectedImage,
+  StartupStrokeImage,
+  EmploymentFillSelectedImage,
+  EmploymentFillImage,
+  EmploymentStrokeSelectedImage,
+  EmploymentStrokeImage,
+  ProductivityFillSelectedImage,
+  ProductivityFillImage,
+  ProductivityStrokeSelectedImage,
+  ProductivityStrokeImage,
+  LifestyleFillSelectedImage,
+  LifestyleFillImage,
+  LifestyleStrokeSelectedImage,
+  LifestyleStrokeImage,
+  HealthFillSelectedImage,
+  HealthFillImage,
+  HealthStrokeSelectedImage,
+  HealthStrokeImage,
+  MindFillSelectedImage,
+  MindFillImage,
+  MindStrokeSelectedImage,
+  MindStrokeImage,
+  HobbyFillSelectedImage,
+  HobbyFillImage,
+  HobbyStrokeSelectedImage,
+  HobbyStrokeImage,
+  LanguageFillSelectedImage,
+  LanguageFillImage,
+  LanguageStrokeSelectedImage,
+  LanguageStrokeImage,
+};
