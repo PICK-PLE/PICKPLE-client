887116eefbd8cb61574a3826d26d6a3fee00f5de
장정안 <wjddks96@naver.com>
2024-07-12 19:15:15 +0900

    feat: HostMyPage ui 구현

---
+++ src/pages/myPage/page/HostMyPage/HostMyPage.style.ts
@@ -0,0 +1,69 @@
+import { Theme, css } from '@emotion/react';
+import { flexGenerator } from '@styles/generator';
+
+export const hostMyPageLayout = css`
+  ${flexGenerator('column', 'center', 'stretch')};
+  height: 4.4rem;
+`;
+
+export const tapLine = (theme: Theme) => css`
+  width: 100%;
+  height: 0.1rem;
+
+  ${theme.color.lightgray1}
+`;
+
+export const tapContainer = (theme: Theme) => css`
+  ${flexGenerator('row', 'center', 'center')}
+  width: 50%;
+  height: 4.4rem;
+  padding: 0.8rem;
+  gap: 0.8rem;
+  flex-shrink: 0;
+
+  background: ${theme.color.white};
+
+  cursor: pointer;
+`;
+
+export const activeTabStyle = (theme: Theme) => css`
+  color: ${theme.color.purple1};
+  border-bottom: 0.1rem solid ${theme.color.purple1};
+`;
+
+export const inactiveTabStyle = (theme: Theme) => css`
+  color: ${theme.color.lightgray2};
+  border-bottom: 0.1rem solid ${theme.color.lightgray2};
+`;
+
+export const tabWrapper = css`
+  ${flexGenerator()}
+`;
+
+export const getTabStyle = (isActive: boolean) => (theme: Theme) =>
+  css`
+    ${tapContainer(theme)}
+    ${isActive ? activeTabStyle(theme) : inactiveTabStyle(theme)}
+    ${theme.font['subhead02-sb-16']}
+  `;
+
+export const hostMyClassCardContainer = css`
+  ${flexGenerator('column')};
+  padding: 3rem 1.8rem;
+  gap: 1rem;
+`;
+
+export const hostMyPageContainer = css`
+  ${flexGenerator('column')};
+  gap: 3rem;
+`;
+
+export const hostMyPageBackground = (theme: Theme) => css`
+  background-color: ${theme.color.background};
+  min-height: 100dvh;
+`;
+
+export const IcHostMypageStyle = css`
+  width: 15rem;
+  height: 15rem;
+`;
--- src/pages/myPage/page/HostMyPage/HostMyPage.tsx
+++ src/pages/myPage/page/HostMyPage/HostMyPage.tsx
@@ -1,10 +1,47 @@
 import { Header } from '@components';
+import {
+  getTabStyle,
+  hostMyClassCardContainer,
+  hostMyPageBackground,
+  hostMyPageLayout,
+  tabWrapper,
+  tapLine,
+} from './HostMyPage.style';
+import { useState } from 'react';
+import { HostMyPageEmptyView } from '@pages/myPage/components';
+import { hostMyClassCardData } from 'src/constants/mocks/HostMyClassCardData';
+import { HostMyClassCard } from '@pages/host/components';
 
 const HostMyPage = () => {
+  const [activeTab, setActiveTab] = useState<'진행 중' | '완료'>('진행 중');
+
   return (
-    <>
+    <div css={hostMyPageBackground}>
       <Header title="my 클래스 모임" />
-    </>
+      <article css={hostMyPageLayout}>
+        <div css={tapLine} />
+        <div css={tabWrapper}>
+          <div css={getTabStyle(activeTab === '진행 중')} onClick={() => setActiveTab('진행 중')}>
+            진행 중
+          </div>
+          <div css={getTabStyle(activeTab === '완료')} onClick={() => setActiveTab('완료')}>
+            완료
+          </div>
+        </div>
+      </article>
+
+      {hostMyClassCardData.length !== 0 ? (
+        <HostMyPageEmptyView
+          text={activeTab === '진행 중' ? '아직 신청한 모임이 없어요' : '아직 참가한 모임이 없어요'}
+        />
+      ) : (
+        <div css={hostMyClassCardContainer}>
+          {hostMyClassCardData.map((data) => (
+            <HostMyClassCard key={data.moimId} hostMyClassData={data} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
