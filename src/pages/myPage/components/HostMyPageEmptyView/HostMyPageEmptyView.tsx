d76d37ec343c612d1c94794da258566d019d5995
장정안 <wjddks96@naver.com>
2024-07-13 16:52:42 +0900

    feat: react-fragment 삭제

--- src/pages/myPage/components/HostMyPageEmptyView/HostMyPageEmptyView.tsx
+++ src/pages/myPage/components/HostMyPageEmptyView/HostMyPageEmptyView.tsx
@@ -9,18 +9,16 @@ import { Button } from '@components';
 
 const HostMyPageEmptyView = () => {
   return (
-    <>
-      <div css={hostMyPageEmptyViewContainer}>
-        <img css={imageStyle} src={graphicImage.HostApplyImage} alt="HostMyPageImage" />
-        <div css={hostMyPageEmptyViewWrapper}>
-          <p css={textStyle}>아직 호스트로 등록되어 있지 않아요</p>
-          <p css={textStyle}>호스트가 되어 다양한 게스트들을 만나 보세요!</p>
-        </div>
-        <Button variant="round" onClick={() => {}}>
-          호스트 신청하러 가기
-        </Button>
+    <div css={hostMyPageEmptyViewContainer}>
+      <img css={imageStyle} src={graphicImage.HostApplyImage} alt="HostMyPageImage" />
+      <div css={hostMyPageEmptyViewWrapper}>
+        <p css={textStyle}>아직 호스트로 등록되어 있지 않아요</p>
+        <p css={textStyle}>호스트가 되어 다양한 게스트들을 만나 보세요!</p>
       </div>
-    </>
+      <Button variant="round" onClick={() => {}}>
+        호스트 신청하러 가기
+      </Button>
+    </div>
   );
 };
 
