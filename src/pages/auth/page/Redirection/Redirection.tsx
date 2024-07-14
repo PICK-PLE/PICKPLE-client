5fa2be81a75fd1ea22b1e9def8a1e399fe190caf
Yoo TaeSeung <remicon99@gmail.com>
2024-07-14 17:25:58 +0900

    feat: access token 설정중

--- src/pages/auth/page/Redirection/Redirection.tsx
+++ src/pages/auth/page/Redirection/Redirection.tsx
@@ -1,13 +1,24 @@
 import { usePostKakaoLogin } from '@apis/domains/user';
+import { userAtom } from '@stores';
+import { useAtom } from 'jotai';
 import { useEffect } from 'react';
 
 const Redirection = () => {
   const code: string = new URL(window.location.href).searchParams.get('code') || '';
   window.history.forward();
-  const { mutate } = usePostKakaoLogin();
+  const { mutate, data } = usePostKakaoLogin();
+  const [user, setUser] = useAtom(userAtom);
+
   useEffect(() => {
     mutate(code);
   }, [code, mutate]);
+
+  useEffect(() => {
+    if (data) {
+      console.log('data', data);
+    }
+    
+  }, [data]);
   return <div></div>;
 };
 
