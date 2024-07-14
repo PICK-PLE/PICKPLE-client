b9a7078c65097e7a6dedd92f67498247789c67ef
장정안 <wjddks96@naver.com>
2024-07-15 01:35:36 +0900

    feat: HostMyClass 내 진행중, 완료 클릭 시 GET 요청

--- src/pages/host/page/HostMyClass/HostMyClass.tsx
+++ src/pages/host/page/HostMyClass/HostMyClass.tsx
@@ -12,8 +12,28 @@ import { hostMyClassCardData } from 'src/constants/mocks/HostMyClassCardData';
 import { HostMyClassCard } from '@pages/host/components';
 
 import { Header } from '@components';
+import { useFetchHostMoimInfo } from '@apis/domains/moim/useFetchHostMoimInfo';
 const HostMyClass = () => {
   const [activeTab, setActiveTab] = useState<'진행 중' | '완료'>('진행 중');
+  const [moimState, setMoimState] = useState<'ongoing' | 'completed'>('ongoing');
+
+  const handleOngoingTabClick = () => {
+    setActiveTab('진행 중');
+    setMoimState('ongoing');
+  };
+
+  const handleCompletedTabClick = () => {
+    setActiveTab('완료');
+    setMoimState('completed');
+  };
+
+  /**@정안TODO 현재 서버에서 hostId는 1로만 구현하랍니다 */
+  const hostId = 1;
+  const { data } = useFetchHostMoimInfo(hostId, moimState);
+
+  if (!data) {
+    return <div>no data</div>;
+  }
 
   return (
     <div css={hostMyClassBackground}>
@@ -21,10 +41,10 @@ const HostMyClass = () => {
       <article css={hostMyClassLayout}>
         <div css={tapLine} />
         <div css={tabWrapper}>
-          <div css={getTabStyle(activeTab === '진행 중')} onClick={() => setActiveTab('진행 중')}>
+          <div css={getTabStyle(activeTab === '진행 중')} onClick={handleOngoingTabClick}>
             진행 중
           </div>
-          <div css={getTabStyle(activeTab === '완료')} onClick={() => setActiveTab('완료')}>
+          <div css={getTabStyle(activeTab === '완료')} onClick={handleCompletedTabClick}>
             완료
           </div>
         </div>
@@ -33,7 +53,7 @@ const HostMyClass = () => {
           <HostMyClassEmptyView text="아직 진행 중인 모임이 없어요" />
         ) : (
           <div css={hostMyClassCardContainer}>
-            {hostMyClassCardData.map((data) => (
+            {data.data.map((data) => (
               <HostMyClassCard key={data.moimId} hostMyClassData={data} />
             ))}
           </div>
