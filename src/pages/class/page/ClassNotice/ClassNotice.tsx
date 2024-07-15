bfb43b5cd65e03185a6e87e79a64c5fdf0608d33
장정안 <wjddks96@naver.com>
2024-07-16 06:40:24 +0900

    feat: ClassNotice 페이지 내 GET & PUT 로직 담고 게시하기 버튼 클릭 시 순서대로 실행

--- src/pages/class/page/ClassNotice/ClassNotice.tsx
+++ src/pages/class/page/ClassNotice/ClassNotice.tsx
@@ -5,11 +5,18 @@ import {
   noticePostLayout,
   noticePostMain,
 } from './ClassNotice.style';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+import { PutImageUploadParams, usePutS3Upload } from '@apis/domains/presignedUrl/usePutS3Upload';
+import { getPresignedUrl } from '@apis/domains/presignedUrl/useFetchPresignedUrl';
 
 const ClassNotice = () => {
   const [noticeTitle, setNoticeTitle] = useState('');
   const [noticeContent, setNoticeContent] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const queryClient = useQueryClient();
+  const putS3UploadMutation = usePutS3Upload();
 
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNoticeTitle(e.target.value);
@@ -17,6 +24,30 @@ const ClassNotice = () => {
 
   const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setNoticeContent(e.target.value);
+  };
+
+  useEffect(() => {
+    setIsButtonDisabled(!(noticeTitle.trim() && noticeContent.trim()));
+  }, [noticeTitle, noticeContent]);
+
+  const handleFileSelect = (files: File[]) => {
+    setSelectedFiles(files);
+  };
+
+  const handleUpload = async () => {
+    if (selectedFiles.length > 0) {
+      // presigned URL 요청
+      const presignedUrls = await queryClient.fetchQuery({
+        queryKey: ['presignedUrl', selectedFiles.length],
+        queryFn: () => getPresignedUrl(1),
+      });
+
+      if (presignedUrls && presignedUrls.length > 0) {
+        const presignedUrl = presignedUrls[0].url;
+        const file = selectedFiles[0];
+        await putS3UploadMutation.mutateAsync({ presignedUrl, file } as PutImageUploadParams);
+      }
+    }
   };
 
   return (
@@ -38,12 +69,19 @@ const ClassNotice = () => {
             placeholder="내용을 입력해주세요"
             maxLength={500}
             size="medium"
+            isValid={true}
           />
           <div css={imageSelectWrapper}>
-            <ImageSelect />
+            <ImageSelect onFileSelect={handleFileSelect} />
           </div>
         </main>
-        <Button variant="large">게시하기</Button>
+
+        <Button
+          variant="large"
+          disabled={isButtonDisabled || selectedFiles.length === 0}
+          onClick={handleUpload}>
+          게시하기
+        </Button>
       </div>
     </div>
   );
