c697fc152dfff1236630726df3b3799b7324840d
장정안 <wjddks96@naver.com>
2024-07-16 06:39:34 +0900

    feat: PUT 로직에 axios instance 빼고 일반 axios put으로 수정

---
+++ src/apis/domains/presignedUrl/usePutS3Upload.ts
@@ -0,0 +1,40 @@
+import { QUERY_KEY } from '@apis/queryKeys/queryKeys';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+
+export interface PutImageUploadParams {
+  presignedUrl: string;
+  file: File;
+}
+
+const putImageUpload = async ({ presignedUrl, file }: PutImageUploadParams) => {
+  try {
+    const response = await axios.put(presignedUrl, file, {
+      headers: {
+        'Content-Type': file.type,
+      },
+    });
+
+    if (!response) {
+      return null;
+    }
+
+    console.log('presigned Put response.data', response.data);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+  return null;
+};
+
+export const usePutS3Upload = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ presignedUrl, file }: PutImageUploadParams) =>
+      putImageUpload({ presignedUrl, file }),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.PRE_SIGNED_URL] });
+    },
+  });
+};
