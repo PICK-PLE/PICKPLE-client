00b0607990b61b9514f99e86447c8ee5c19d9ca2
TaeSeung Yoo <59465914+gudusol@users.noreply.github.com>
2024-07-15 23:31:39 +0900

    [Feat/#158] Home API 연동 (#159)
    * feat: home API 연동(배너, 카테고리)
    
    * fix: 카테고리 get 함수 이름 수정
    
    * feat: import/export 파일 추가

--- src/apis/domains/moim/useFetchMoimCategories.ts
+++ src/apis/domains/moim/useFetchMoimCategories.ts
@@ -1,21 +1,25 @@
 import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEY } from '@apis/queryKeys/queryKeys';
 import { get } from '@apis/api';
+import { ApiResponseType } from '@types';
 
-const getMoimBanner = async () => {
-  const response = await get('/moim/categories');
-
-  if (!response) {
+const getMoimCategories = async (): Promise<string[] | null> => {
+  try {
+    const response = await get<ApiResponseType<string[]>>('/moim/categories');
+    return response.data.data;
+  } catch (error) {
+    console.error('An error occurred while fetching the category:', error);
     return null;
   }
-  return response.data;
 };
 
 export const useFetchMoimCategories = () => {
   return useQuery({
     queryKey: [QUERY_KEY.MOIM_CATEGORIES],
-    queryFn: () => getMoimBanner(),
-    // staleTime: 3000,
-    // gcTime: 50000,
+    queryFn: () => getMoimCategories(),
+    staleTime: 1000 * 60 * 60 * 24, // 24시간
+    gcTime: 1000 * 60 * 60 * 24 * 7, // 1주일
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
   });
 };
--- src/apis/queryKeys/queryKeys.ts
+++ src/apis/queryKeys/queryKeys.ts
@@ -3,4 +3,5 @@ export const QUERY_KEY = {
   TEST: 'test',
   KAKAO_LOGIN: 'kakaoLogin',
   MOIM_CATEGORIES: 'moimCategories',
+  MOIM_BANNER: 'moimBanner',
 } as const;
