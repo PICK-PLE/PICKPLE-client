7920f0ce1b2252e3f2fe0570cab8d170e3c947c7
장정안 <wjddks96@naver.com>
2024-07-16 22:59:02 +0900

    feat: imageUpload util 함수 타입 및 구조분해 할당

--- src/utils/imageUpload.ts
+++ src/utils/imageUpload.ts
@@ -1,30 +1,36 @@
 import { QueryClient } from '@tanstack/react-query';
 import { PutImageUploadParams } from '@apis/domains/presignedUrl/usePutS3Upload';
-import { getPresignedUrl } from '@apis/domains/presignedUrl/useFetchPresignedUrl';
+import { getPresignedUrl, PresignedUrlType } from '@apis/domains/presignedUrl/useFetchPresignedUrl';
 
 interface UploadParams {
   selectedFiles: File[];
   putS3Upload: (params: PutImageUploadParams) => Promise<void>;
+  type: PresignedUrlType;
 }
 
 export const handleUpload = async ({
   selectedFiles,
   putS3Upload,
+  type,
 }: UploadParams): Promise<string[]> => {
   const queryClient = new QueryClient();
+  const s3UrlList = [];
   if (selectedFiles.length > 0) {
     const presignedUrls = await queryClient.fetchQuery({
       queryKey: ['presignedUrl', selectedFiles.length],
-      queryFn: () => getPresignedUrl(selectedFiles.length),
+      queryFn: () => getPresignedUrl(selectedFiles.length, type),
     });
 
     if (presignedUrls && presignedUrls.length > 0) {
       for (let i = 0; i < selectedFiles.length; i++) {
-        const presignedUrl = presignedUrls[i].url;
+        const { fileName, url } = presignedUrls[i];
         const file = selectedFiles[i];
+        const baseUrl = url?.split(type.toString())[0]; //최종 파일 경로
+        const imageUrl = `${baseUrl}${type}/${fileName}`;
+        s3UrlList.push(imageUrl);
+
         try {
-          const response = await putS3Upload({ presignedUrl, file } as PutImageUploadParams);
-          return response;
+          await putS3Upload({ url, file } as PutImageUploadParams);
         } catch (err) {
           console.error(`${file.name}이 업로드에 실패했음`, err);
           return [];
@@ -32,4 +38,5 @@ export const handleUpload = async ({
       }
     }
   }
+  return s3UrlList;
 };
