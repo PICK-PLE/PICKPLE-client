b9d61fd2366dd5b51d6058b03b0537059e9a4ebf
장정안 <wjddks96@naver.com>
2024-07-17 05:51:24 +0900

    feat: useEffect 제거 테스트 불가 (서버에서 모임데이터를 전부 지운 이슈)

--- src/pages/host/page/HostMyClass/HostMyClass.tsx
+++ src/pages/host/page/HostMyClass/HostMyClass.tsx
@@ -6,16 +6,25 @@ import {
   tabWrapper,
   tapLine,
 } from './HostMyClass.style';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { HostMyClassEmptyView } from '@pages/host/components';
 import { hostMyClassCardData } from 'src/constants/mocks/HostMyClassCardData';
 import { HostMyClassCard } from '@pages/host/components';
 
 import { Header } from '@components';
 import { useFetchHostMoimInfo } from '@apis/domains/moim/useFetchHostMoimInfo';
+import { useAtom } from 'jotai';
+import { userAtom } from '@stores';
 const HostMyClass = () => {
   const [activeTab, setActiveTab] = useState<'진행 중' | '완료'>('진행 중');
   const [moimState, setMoimState] = useState<'ongoing' | 'completed'>('ongoing');
+
+  const [{ hostId }] = useAtom(userAtom);
+
+  /**@정안TODO 현재 서버에서 모임을 전부 제거한 이슈로 useEffect 없어도 되는지 테스트 불가 */
+  useEffect(() => {
+    setMoimState(!activeTab ? 'ongoing' : 'completed');
+  }, [moimState, activeTab, hostId]);
 
   const handleOngoingTabClick = () => {
     setActiveTab('진행 중');
@@ -27,9 +36,10 @@ const HostMyClass = () => {
     setMoimState('completed');
   };
 
-  /**@정안TODO 현재 서버에서 hostId는 1로만 구현하랍니다 */
-  const hostId = 1;
-  const { data } = useFetchHostMoimInfo(hostId, moimState);
+  /**@정안TODO 장정안의 hostId는 5
+   * 현재 hostId가 0찍혔다가 null로 오는 이슈가 있음.
+   */
+  const { data } = useFetchHostMoimInfo(5, moimState || 'ongoing');
 
   if (!data) {
     return <div>no data</div>;
@@ -53,7 +63,7 @@ const HostMyClass = () => {
           <HostMyClassEmptyView text="아직 진행 중인 모임이 없어요" />
         ) : (
           <div css={hostMyClassCardContainer}>
-            {data.data.map((data) => (
+            {data.map((data) => (
               <HostMyClassCard key={data.moimId} hostMyClassData={data} />
             ))}
           </div>
