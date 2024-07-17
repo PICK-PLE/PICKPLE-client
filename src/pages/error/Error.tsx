92f12301720a8cbc736fbe72479fe11577f491ed
장정안 <wjddks96@naver.com>
2024-07-17 23:17:58 +0900

    feat: navigate 속성 중 replace true 추가

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,26 +1,41 @@
 import { Button, LogoHeader } from '@components';
-import { buttonStyle, errorBox, errorIconStyle, errorWrapper, textStyle } from './Error.style';
-import { graphicImage } from '@constants';
-import { useEasyNavigate } from '@hooks';
+import {
+  buttonStyle,
+  errorBox,
+  errorIconStyle,
+  errorWrapper,
+  textStyle,
+  errorContainer,
+} from './Error.style';
+import { graphicImage, routePath } from '@constants';
+import { useNavigate } from 'react-router-dom';
 
 const Error = () => {
-  const { goHome } = useEasyNavigate();
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate(routePath.HOME, {
+      replace: true,
+    });
+  };
+
   return (
     <>
       <LogoHeader isIcon={false} />
-
-      <section css={errorWrapper}>
-        <img src={graphicImage.ErrorImage} alt="에러 아이콘" css={errorIconStyle} />
-        <div css={errorBox}>
-          <p css={textStyle}>오류가 발생했어요</p>
-          <p css={textStyle}>잠시 후 다시 시도해 주세요</p>
-        </div>
-        <div css={buttonStyle}>
-          <Button variant="round" onClick={goHome}>
-            홈으로 돌아가기
-          </Button>
-        </div>
-      </section>
+      <div css={errorContainer}>
+        <section css={errorWrapper}>
+          <img src={graphicImage.ErrorImage} alt="에러 아이콘" css={errorIconStyle} />
+          <div css={errorBox}>
+            <p css={textStyle}>오류가 발생했어요</p>
+            <p css={textStyle}>잠시 후 다시 시도해 주세요</p>
+          </div>
+          <div css={buttonStyle}>
+            <Button variant="round" onClick={handleButtonClick}>
+              홈으로 돌아가기
+            </Button>
+          </div>
+        </section>
+      </div>
     </>
   );
 };
