d34f5cfa2c293d58f2ab2fb4e39807c053e9c023
장정안 <wjddks96@naver.com>
2024-07-18 04:44:51 +0900

    feat: ErrorBoundary 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Global, ThemeProvider } from '@emotion/react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import theme from './styles/theme';
 import GlobalStyle from './styles/global';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import {
   authRoutes,
@@ -29,9 +28,8 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
         <RouterProvider router={router} />
@@ -39,7 +37,7 @@ const App = () => {
       <div style={{ fontSize: '16px' }}>
         <ReactQueryDevtools />
       </div>
-    </QueryClientProvider>
+    </>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+import Error from '@pages/error/Error.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const queryClient = new QueryClient({});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary onReset={() => reset()} FallbackComponent={Error}>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
+  </QueryClientProvider>
 );
