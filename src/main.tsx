c1861ef5e5d08ec03166075bb008403d42f365e5
장정안 <wjddks96@naver.com>
2024-07-18 08:02:05 +0900

    feat: main.tsx에서 설정한 에러바운더리 & 서스펜스 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from '@tanstack/react-query';
-import { ErrorBoundary } from 'react-error-boundary';
-import Error from '@pages/error/Error.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,13 +14,7 @@ const queryClient = new QueryClient({
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <QueryErrorResetBoundary>
-        <ErrorBoundary fallback={<Error />}>
-          <App />
-        </ErrorBoundary>
-      </QueryErrorResetBoundary>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
