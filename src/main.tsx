882c551223b4ab14ca0c986104bb1356fbe51af5
장정안 <wjddks96@naver.com>
2024-07-18 05:47:45 +0900

    feat: main.tsx에 errorboundary 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+import Error from '@pages/error/Error.tsx';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      throwOnError: true,
+    },
+    mutations: {
+      throwOnError: true,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <QueryErrorResetBoundary>
+        <ErrorBoundary fallback={<Error />}>
+          <App />
+        </ErrorBoundary>
+      </QueryErrorResetBoundary>
     </QueryClientProvider>
   </React.StrictMode>
 );
