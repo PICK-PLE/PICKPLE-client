7058135b399c1cfa5caf8596fdcb95b59216afde
imhwarang <imhwarang4@naver.com>
2024-07-19 04:26:43 +0900

    refactor: 코리 반영

--- src/components/common/inputs/AccountNumberInput/AccountNumberInput.tsx
+++ src/components/common/inputs/AccountNumberInput/AccountNumberInput.tsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import Input from '../Input/Input';
-
-// interface AccountNumberInputProps {
-//   value: number;
-//   onChange: (value: number) => void;
-// }
-
-// const AccountNumberInput = ({ value, onChange }: AccountNumberInputProps) => {
-//   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const numericValue = e.target.value.replace(/[^0-9]/g, '');
-//     onChange(Number(numericValue));
-//   };
-//   return <Input value={value.toString()} inputLabel='계좌 번호' placeholder="'-' 없이 입력" onChange={handleInputChange} isValid={true} isCountValue={false} />;
-// };
-
-// export default AccountNumberInput;
-
 import React from 'react';
 import Input from '../Input/Input';
 
