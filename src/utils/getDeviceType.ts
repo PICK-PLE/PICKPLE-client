d6b1805fd7d963a72b8f51babce22938026dd29b
Parkchaeyeon <ccyy1029@naver.com>
2024-07-19 05:32:12 +0900

    refactor: 코드리뷰 - 필요없는 주석 삭제

--- src/utils/getDeviceType.ts
+++ src/utils/getDeviceType.ts
@@ -18,15 +18,3 @@ declare global {
   
     return 'Desktop';
   };
-
-// 이런 식으로 사용할 것
-//   const handleButtonClick = () => {
-//     const deviceType = getDeviceType();
-//     if (deviceType === 'Android') {
-//       console.log('안드로이드 기기에서 버튼 클릭');
-//     } else if (deviceType === 'iOS') {
-//       console.log('아이폰에서 버튼 클릭');
-//     } else {
-//       console.log('데스크탑에서 버튼 클릭');
-//     }
-//   };
