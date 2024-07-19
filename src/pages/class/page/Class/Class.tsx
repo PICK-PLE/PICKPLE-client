8b60f51d3db64750135f2f8f76e3d6859141d844
장정안 <wjddks96@naver.com>
2024-07-20 05:43:52 +0900

    fix: 공유하기 버튼 클릭 시 토스트 뜨는 로직 수정

--- src/pages/class/page/Class/Class.tsx
+++ src/pages/class/page/Class/Class.tsx
@@ -87,7 +87,7 @@ const Class = () => {
 
   const handleShareButtonClick = async () => {
     const shareSuccess = await handleShare(url, shareTitle, text, handleCopyToClipboard);
-    if (!shareSuccess) {
+    if (shareSuccess === false) {
       showToast();
     }
   };
