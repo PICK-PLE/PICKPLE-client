4ef549eb12135f585e6594a06a5862fdc8336fb8
장정안 <wjddks96@naver.com>
2024-09-05 19:22:12 +0900

    feat: ClassNotice.tsx에서 navigate 함수 추가

--- src/pages/class/components/ClassNotice/ClassNotice.tsx
+++ src/pages/class/components/ClassNotice/ClassNotice.tsx
@@ -1,19 +1,29 @@
 import { noticeCardContainer } from './ClassNotice.style';
 import { NoticeCard } from '..';
-
 import { components } from '@schema';
+import { useNavigate } from 'react-router-dom';
 
 type NoticeListGetByMoimResponse = components['schemas']['NoticeListGetByMoimResponse'];
 
 interface ClassNoticeProps {
   noticeData: NoticeListGetByMoimResponse[];
+  moimId: string;
 }
 
-const ClassNotice = ({ noticeData }: ClassNoticeProps) => {
+const ClassNotice = ({ noticeData, moimId }: ClassNoticeProps) => {
+  const navigate = useNavigate();
+  const handleNoticeCardClick = (noticeId: number) => {
+    navigate(`/class/${moimId}/notice/${noticeId.toString()}`);
+  };
+
   return (
     <div css={noticeCardContainer}>
       {noticeData.map((data) => (
-        <NoticeCard key={data.noticeId} noticeData={data} />
+        <NoticeCard
+          key={data.noticeId}
+          noticeData={data}
+          onClick={() => handleNoticeCardClick(data.noticeId ?? 0)}
+        />
       ))}
     </div>
   );
