418d4616c3769fe15f7ad9bcab912b05708ac834
장정안 <wjddks96@naver.com>
2024-09-05 19:19:18 +0900

    feat: 공지사항 상세 리액트 쿼리 연결

---
+++ src/apis/domains/notice/useFetchMoimNoticeDetail.ts
@@ -0,0 +1,31 @@
+import { get } from '@apis/api';
+import { QUERY_KEY } from '@apis/queryKeys/queryKeys';
+import { components } from '@schema';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { ApiResponseType } from '@types';
+
+type NoticeDetailGetResponse = components['schemas']['NoticeDetailGetResponse'];
+
+const getMoimNoticeDetail = async (
+  moimId: string,
+  noticeId: string
+): Promise<NoticeDetailGetResponse | null> => {
+  try {
+    const response = await get<ApiResponseType<NoticeDetailGetResponse>>(
+      `/v2/moim/${moimId}/notice/${noticeId}`
+    );
+    return response.data.data;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+export const useFetchMoimNoticeDetail = (moimId: string, noticeId: string) => {
+  return useSuspenseQuery({
+    queryKey: [QUERY_KEY.MOIM_NOTICE_DETAIL, moimId, noticeId],
+    queryFn: () => getMoimNoticeDetail(moimId, noticeId),
+    staleTime: 1000 * 60, // useFetchMoimNoticeList와 staleTime 맞춤
+    gcTime: 1000 * 60 * 5, // useFetchMoimNoticeList와 staleTime 맞춤
+  });
+};
