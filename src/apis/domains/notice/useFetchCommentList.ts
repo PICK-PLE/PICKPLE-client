2e132c3798e94ed0d7afe9f7abab53dfb3148dd5
장정안 <wjddks96@naver.com>
2024-09-06 15:25:36 +0900

    feat: 공지사항 댓글 리스트 api 함수 작성

---
+++ src/apis/domains/notice/useFetchCommentList.ts
@@ -0,0 +1,28 @@
+import { get } from '@apis/api';
+import { QUERY_KEY } from '@apis/queryKeys/queryKeys';
+import { components } from '@schema';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { ApiResponseType } from '@types';
+
+type CommentGetResponse = components['schemas']['CommentGetResponse'];
+
+const getCommentList = async (noticeId: string): Promise<CommentGetResponse[] | null> => {
+  try {
+    const response = await get<ApiResponseType<CommentGetResponse[]>>(
+      `/v2/notice/${noticeId}/comment-list`
+    );
+    return response.data.data;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+export const useFetchCommentList = (noticeId: string) => {
+  return useSuspenseQuery({
+    queryKey: [QUERY_KEY.COMMENT_LIST, noticeId],
+    queryFn: () => getCommentList(noticeId),
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 10,
+  });
+};
