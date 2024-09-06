80edf7c319e084b52521297b41ce67fe0b01d82f
장정안 <wjddks96@naver.com>
2024-09-06 15:27:39 +0900

    feat: 삭제 모달 구현

---
+++ src/pages/class/components/DeleteModal/DeleteModal.tsx
@@ -0,0 +1,37 @@
+import {
+  buttonWrapper,
+  cancelButtonStyle,
+  cancelButtonText,
+  deleteButtonStyle,
+  deleteButtonText,
+  deleteModalContainer,
+  deleteModalWrapper,
+  detailWrapper,
+  title,
+} from './DeleteModal.style';
+
+interface DeleteModalProps {
+  onClose: () => void;
+}
+
+const DeleteModal = ({ onClose }: DeleteModalProps) => {
+  return (
+    <article css={deleteModalContainer}>
+      <div css={deleteModalWrapper}>
+        <div css={detailWrapper}>
+          <p css={title}>삭제하시겠습니까?</p>
+          <div css={buttonWrapper}>
+            <button css={cancelButtonStyle} onClick={onClose}>
+              <p css={cancelButtonText}>취소</p>
+            </button>
+            <button css={deleteButtonStyle}>
+              <p css={deleteButtonText}>삭제</p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default DeleteModal;
