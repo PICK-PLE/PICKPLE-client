4e6bf1d19dc0b3b140ad18162f9632c2dd487907
thisishwarang <101498590+thisishwarang@users.noreply.github.com>
2024-09-08 22:01:32 +0900

    [Feat/#256] 리뷰 작성 페이지 퍼블리싱 완료 (#257)
    * feat: 리뷰 작성 페이지 뷰 완성
    
    * feat: 리뷰 작성 페이지 태그 클릭되도록 수정
    
    * feat: 리뷰 작성 완료 페이지 퍼블리싱 완료
    
    * feat: 태그 총 3개만 선택할 수 있도록 수정
    
    * feat: 커서 포인터 추가
    
    * feat: 리뷰 태그 3개 이상 클릭시 토스트 띄우기 구현
    
    * fix: 코드리뷰 반영
    
    * fix: 코드리뷰 반영

---
+++ src/components/common/TagSelectBox/TagSelectBox.style.ts
@@ -0,0 +1,27 @@
+import { Theme, css } from '@emotion/react';
+
+export const tagWrapper = css`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem 0.6rem;
+  width: 100%;
+`;
+
+export const buttonStyle = (theme: Theme) => css`
+  padding: 0.5rem;
+
+  border-radius: 5px;
+  border: 1px solid ${theme.color.purple1};
+  ${theme.font['body03-r-12']}
+
+  cursor: pointer;
+`;
+
+export const selectedStyle = (theme: Theme) => css`
+  color: ${theme.color.white};
+  background-color: ${theme.color.purple1};
+`;
+export const unSelectedStyle = (theme: Theme) => css`
+  color: ${theme.color.purple1};
+  background-color: ${theme.color.white};
+`;
--- src/constants/images/graphics.ts
+++ src/constants/images/graphics.ts
@@ -8,4 +8,4 @@ export const HostMyPageImage = '/image/graphics/image_host_mypage.png';
 export const HostApplyFinishImage = '/image/graphics/image_hostapply_finish.png';
 export const LoginImage = '/image/graphics/image_login.png';
 export const NoticeImage = '/image/graphics/image_notice.png';
-export const ReviewImage = '/image/graphics/image_review.png';
+export const ReviewFinishImage = '/image/graphics/image_review_finish.png';
