54ec57b612e8a4c9f53dbddea4308f2371abd007
장정안 <wjddks96@naver.com>
2024-09-11 15:26:16 +0900

    feat: 공지사항 댓글 삭제 api 함수 구현

---
+++ src/apis/domains/notice/useDeleteNoticeComment.ts
@@ -0,0 +1,25 @@
+import { del } from '@apis/api';
+import { QUERY_KEY } from '@apis/queryKeys/queryKeys';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+const deleteNoticeComment = async (noticeId: string, commentId: string) => {
+  try {
+    const response = await del(`/v2/notice/${noticeId}/comment/${commentId}`);
+
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+export const useDeleteNoticeComment = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ noticeId, commentId }: { noticeId: string; commentId: string }) =>
+      deleteNoticeComment(noticeId, commentId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.NOTICE_COMMENT] });
+    },
+  });
+};
