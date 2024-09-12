b6790b0055a06d08a20199f51a09f1e636a17a23
장정안 <wjddks96@naver.com>
2024-09-12 21:59:33 +0900

    feat: 게스트 마이페이지 GET api 연결

---
+++ src/apis/domains/guest/useFetchGuestInfo.ts
@@ -0,0 +1,25 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import { get } from '@apis/api';
+import { QUERY_KEY } from '@apis/queryKeys/queryKeys';
+
+import { components } from '@schema';
+import { ApiResponseType } from '@types';
+
+type GuestGetResponse = components['schemas']['GuestGetResponse'];
+const getGuestInfo = async (guestId: string): Promise<GuestGetResponse | null> => {
+  try {
+    const response = await get<ApiResponseType<GuestGetResponse>>(`/v2/guest/${guestId}`);
+    return response.data.data;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+export const useFetchGuestInfo = (guestId: string) => {
+  return useSuspenseQuery({
+    queryKey: [QUERY_KEY.GUEST_INFO],
+    queryFn: () => getGuestInfo(guestId),
+  });
+};
