f9f6db2c5e4b89e1f1e1890e7b378c7f1eed73bd
장정안 <wjddks96@naver.com>
2024-09-13 15:30:41 +0900

    feat: 댓글 삭제 화면 실시간 반영 구현

--- src/pages/class/components/DeleteModal/DeleteModal.tsx
+++ src/pages/class/components/DeleteModal/DeleteModal.tsx
@@ -1,4 +1,7 @@
+import { useDeleteNoticeComment } from '@apis/domains/notice/useDeleteNoticeComment';
+
 import { Button } from '@components';
+
 import {
   buttonWrapper,
   cancelButtonStyle,
@@ -10,27 +13,35 @@ import {
   detailWrapper,
   title,
 } from './DeleteModal.style';
-import { useDeleteNoticeComment } from '@apis/domains/notice/useDeleteNoticeComment';
 
 interface DeleteModalProps {
   onClose: () => void;
+  commentId?: number;
+  noticeId?: string | undefined;
 }
 
-const DeleteModal = ({ onClose }: DeleteModalProps) => {
+const DeleteModal = ({ onClose, noticeId, commentId }: DeleteModalProps) => {
   const { mutate: deleteComment } = useDeleteNoticeComment();
+  const handleButtonClick = () => {
+    if (noticeId && commentId !== undefined) {
+      deleteComment({ noticeId: noticeId.toString(), commentId: commentId.toString() });
+      onClose();
+    } else {
+      console.error('댓글을 삭제하는데 에러가 발생했습니다.');
+      alert('댓글을 삭제하는데 에러가 발생했습니다.');
+    }
+  };
+
   return (
     <article css={deleteModalContainer}>
       <div css={deleteModalWrapper}>
         <div css={detailWrapper}>
-          <p css={title}>삭제하시겠습니까?</p>
+          <p css={title}>삭제하시겠습니까?????</p>
           <div css={buttonWrapper}>
             <Button variant="xSmall" onClick={onClose} customStyle={cancelButtonStyle}>
               <p css={cancelButtonText}>취소</p>
             </Button>
-            <Button
-              variant="xSmall"
-              onClick={() => deleteComment({ noticeId, commentId })}
-              customStyle={deleteButtonStyle}>
+            <Button variant="xSmall" onClick={handleButtonClick} customStyle={deleteButtonStyle}>
               <p css={deleteButtonText}>삭제</p>
             </Button>
           </div>
