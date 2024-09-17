52b9e963893a253e63e69929262a12b16beba258
Yoo TaeSeung <remicon99@gmail.com>
2024-09-17 22:57:10 +0900

    fix: 카테고리 API 명 수정

--- src/apis/domains/moim/useFetchMoimCategories.ts
+++ src/apis/domains/moim/useFetchMoimCategories.ts
@@ -9,7 +9,7 @@ import { ApiResponseType } from '@types';
 
 const getMoimCategories = async (): Promise<string[] | null> => {
   try {
-    const response = await get<ApiResponseType<string[]>>('/v1/moim/categories');
+    const response = await get<ApiResponseType<string[]>>('/v2/moim/categories');
     return response.data.data;
   } catch (error) {
     console.error('An error occurred while fetching the category:', error);
