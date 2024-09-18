e035c199d0b6a6a271ab8c25615156816faeeee5
장정안 <wjddks96@naver.com>
2024-09-18 17:25:38 +0900

    feat: 공지사항 댓글 내 호스트 프로필 부분 간격 조정

--- src/pages/class/components/CommentBox/CommentBox.style.ts
+++ src/pages/class/components/CommentBox/CommentBox.style.ts
@@ -15,7 +15,7 @@ export const commentSectionContainer = (theme: Theme) => css`
 export const commentContainer = (isOwner: boolean) => (theme: Theme) => css`
   ${flexGenerator('row', 'space-between', 'flex-start')};
   padding: 1.7rem 1.3rem 1.7rem 2rem;
-  gap: 1rem;
+  gap: 1.2rem;
   background: ${isOwner ? theme.color.bg_white1 : theme.color.white};
   width: 100%;
 `;
