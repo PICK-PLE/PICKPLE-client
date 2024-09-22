bba0a4a0306bb58af1583d573243bbe2f8a1fd0e
Parkchaeyeon <ccyy1029@naver.com>
2024-09-23 03:14:18 +0900

    fix: 배너 순서 다시 원래대로!

--- src/pages/home/page/Home/config.ts
+++ src/pages/home/page/Home/config.ts
@@ -3,10 +3,10 @@ import { homeBanner1, homeBanner2 } from 'src/assets/lotties';
 export const bannerList = [
   {
     id: 1,
-    animationData: homeBanner2,
+    animationData: homeBanner1,
   },
   {
     id: 2,
-    animationData: homeBanner1,
+    animationData: homeBanner2,
   },
 ];
