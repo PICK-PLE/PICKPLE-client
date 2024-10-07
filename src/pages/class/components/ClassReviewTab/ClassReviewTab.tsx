8cacf11b1bb13107fba26789e9c985201786cbd5
장정안 <wjddks96@naver.com>
2024-10-08 01:47:34 +0900

    feat: 리뷰 순서 최신순으로 정렬

--- src/pages/class/components/ClassReviewTab/ClassReviewTab.tsx
+++ src/pages/class/components/ClassReviewTab/ClassReviewTab.tsx
@@ -17,7 +17,14 @@ const ClassReviewTab = ({ moimId }: ClassReviewTabProps) => {
   }
   return (
     <section css={reviewSectionStyle}>
-      {moimReviewList?.map((review, i) => <Review key={i} reviewData={review} />)}
+      {/* moimReviewList안에 date를 이용해서 sort로 최신순으로 정렬 */}
+      {moimReviewList
+        ?.sort((a, b) => {
+          const dateA = a.date ? new Date(a.date).getTime() : 0; // date가 없으면 0으로 설정
+          const dateB = b.date ? new Date(b.date).getTime() : 0; // date가 없으면 0으로 설정
+          return dateB - dateA; // 최신순 정렬
+        })
+        .map((review, i) => <Review key={i} reviewData={review} />)}
     </section>
   );
 };
